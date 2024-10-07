810898bdc100914b98c12c741e77991824b3a135
George <georgekalyvianakis18@gmail.com>
2024-10-07 22:32:12 +0200

    Rename reservation page to Playground for tests

--- client/src/pages/Playground/Playground.tsx
+++ client/src/pages/Playground/Playground.tsx
@@ -6,9 +6,11 @@ import { FieldValues, FormProvider, useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 import EmailField from "../../components/Fields/Email";
 import CalendarField from "../../components/Fields/Calendar";
+import CheckboxField from "../../components/Fields/Checkbox";
+import NumberField from "../../components/Fields/Number";
 
 
-export default function ReservationPage(): ReactElement {
+export default function PlaygroundPage(): ReactElement {
 
   const methods = useForm({
     mode: "all"
@@ -23,27 +25,24 @@ export default function ReservationPage(): ReactElement {
     <div className="reservation">
       <div className="container max-w-[1400px] mx-auto mt-3 px-3">
         <div className="py-4">
-          <h3 className="mb-0 text-xl">Reserve a table</h3>
+          <h3 className="mb-0 text-xl">Playground</h3>
           <p className="mt-0 text-xs text-slate-400">
-            Find the best table for you.
+            Look how kids are having fun. Go join them...
           </p>
         </div>
-
         <div className="flex gap-3 xs:flex-wrap">
           <div>
             <div>
               <FormProvider {...methods}>
                 <form className="mx-auto" onSubmit={methods.handleSubmit(handleSubmit)} noValidate>
                   <div className="flex w-full flex-wrap gap-4">
-                    <div className="flex w-full flex-wrap xl:flex-nowrap gap-4">
-                      <CalendarField name="reservation-date" />
-                      <ReservationTimeField name="reservation-time" />
-                    </div>
-                    <div className="flex w-full flex-wrap xl:flex-nowrap gap-4">
-                      <InputField type="text" label="Name" name="reservation-name" isRequired />
-                      <EmailField name="reservation-email" label="Email" isRequired />
-                    </div>
-                    <Button color="primary" type="submit" variant="flat" >Reserve</Button>
+                    <CalendarField name="reservation-date" />
+                    <ReservationTimeField name="reservation-time" />
+                    <InputField type="text" name="reservation-name" label="Input Field" isRequired />
+                    <EmailField name="email" label="Email" isRequired />
+                    <NumberField name="number" label="Number Field" isRequired />
+                    <CheckboxField name="checkbox" label="Checkbox field" />
+                    <Button color="primary" type="submit" variant="flat" >Submit</Button>
                   </div>
                 </form>
                 <DevTool control={methods.control} />
