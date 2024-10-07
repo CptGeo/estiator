a56a166575cfa0104b7bf33ddbaae923457a7b6d
cptgeo <georgekalyvianakis18@gmail.com>
2024-10-07 21:18:53 +0200

    Add Calendar and ReservationTime fields in Reservation page

--- client/src/pages/Reservation/Reservation.tsx
+++ client/src/pages/Reservation/Reservation.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@nextui-org/react";
 import { ReactElement } from "react";
-import ReservationDate from "../../components/ReservationDate/ReservationDate";
-import ReservationTime from "../../components/ReservationTime/ReservationTime";
+import ReservationTimeField from "../../components/Fields/ReservationTime";
 import InputField from "../../components/Fields/Input";
 import { FieldValues, FormProvider, useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 import EmailField from "../../components/Fields/Email";
+import CalendarField from "../../components/Fields/Calendar";
 
 
 export default function ReservationPage(): ReactElement {
@@ -21,7 +21,7 @@ export default function ReservationPage(): ReactElement {
 
   return (
     <div className="reservation">
-      <div className="container max-w-[650px] mx-auto px-3 pb-5">
+      <div className="container max-w-[1400px] mx-auto mt-3 px-3">
         <div className="py-4">
           <h3 className="mb-0 text-xl">Reserve a table</h3>
           <p className="mt-0 text-xs text-slate-400">
@@ -29,23 +29,28 @@ export default function ReservationPage(): ReactElement {
           </p>
         </div>
 
-        <FormProvider {...methods}>
-          <form className="mx-auto" onSubmit={methods.handleSubmit(handleSubmit)} noValidate>
-            <div className="flex w-full flex-wrap gap-4">
-              <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-                <ReservationDate />
-                <ReservationTime />
-              </div>
-              <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-                <InputField type="text" label="Name" name="name" isRequired />
-                <EmailField name="email" label="Email" isRequired />
-              </div>
-
-              <Button color="primary" type="submit" variant="flat" >Reserve</Button>
+        <div className="flex gap-3 xs:flex-wrap">
+          <div>
+            <div>
+              <FormProvider {...methods}>
+                <form className="mx-auto" onSubmit={methods.handleSubmit(handleSubmit)} noValidate>
+                  <div className="flex w-full flex-wrap gap-4">
+                    <div className="flex w-full flex-wrap xl:flex-nowrap gap-4">
+                      <CalendarField name="reservation-date" />
+                      <ReservationTimeField name="reservation-time" />
+                    </div>
+                    <div className="flex w-full flex-wrap xl:flex-nowrap gap-4">
+                      <InputField type="text" label="Name" name="reservation-name" isRequired />
+                      <EmailField name="reservation-email" label="Email" isRequired />
+                    </div>
+                    <Button color="primary" type="submit" variant="flat" >Reserve</Button>
+                  </div>
+                </form>
+                <DevTool control={methods.control} />
+              </FormProvider>
             </div>
-          </form>
-          <DevTool control={methods.control} />
-        </FormProvider>
+          </div>
+        </div>
       </div>
     </div>
   );
