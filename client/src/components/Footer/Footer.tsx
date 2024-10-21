67a4dd3a314ab02184706be9ad5985424d48ac5e
George <georgekalyvianakis18@gmail.com>
2024-10-21 15:01:18 +0000

    Add logo in Navigation, improve styling

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer(): ReactElement {
     <footer className="bg-gray-700 mt-5">
       <div className="p-3 container mx-auto">
         <div className="flex gap-7">
-            <div className="border-r-2 border-slate-200">
+            <div className="border-r-1 border-slate-500">
               <Image src={logo} className="pl-3 pr-5 bg-none max-w-[200px]" />
             </div>
             <ul className="list-none">
@@ -18,7 +18,7 @@ export default function Footer(): ReactElement {
             <ul className="list-none bg-primary" />
         </div>
       </div>
-      <Divider />
+      <Divider className="bg-slate-500"/>
       <div className="p-3 container mx-auto">
         <div className="drop-shadow-lg text-center text-slate-400 tracking-wider">Copyright © 2024 Estiator.io. All rights reserved.</div>
       </div>
--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -1,13 +1,22 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button, Link, Image } from "@nextui-org/react";
+import logo from "../../assets/images/logo.png";
 
 export default function Navigation() {
   return (
-    <Navbar className="bg-transparent">
+    <Navbar maxWidth="2xl" position="sticky" isBordered className="bg-transparent">
       <NavbarBrand>
-        <p className="font-bold text-inherit">estiator.io</p>
+        <Image src={logo} className="max-w-[150px] px-4" />
       </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem className="lg:flex">
+          <Link href="/" className="text-slate-700 cursor-pointer">Home</Link>
+        </NavbarItem>
+        <NavbarItem className="lg:flex">
+          <Link href="/playground" className="text-slate-700 cursor-pointer">Playground</Link>
+        </NavbarItem>
+      </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
+        <NavbarItem className="lg:flex">
           <Button onClick={() => alert("Login")} variant="light" color="primary">Login</Button>
         </NavbarItem>
         <NavbarItem>
