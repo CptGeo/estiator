21c2d9ef1895ec0f6234d579c590bbd02c182643
George <georgekalyvianakis18@gmail.com>
2024-10-21 15:38:18 +0000

    Add endContent in EmailField

--- client/src/components/Fields/Email.tsx
+++ client/src/components/Fields/Email.tsx
@@ -2,15 +2,24 @@ import { ReactElement } from "react";
 import InputField from "./Input";
 import { RegisterOptions } from "react-hook-form";
 import { ControlledInputProps } from "./types";
+import { MailIcon } from "../Icons/MailIcon";
 
 export default function EmailField(props: Omit<ControlledInputProps, "type">): ReactElement {
+  const defaultRules: RegisterOptions = {
+    pattern: {
+      value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/i,
+      message: "Please provide a valid email",
+    },
+  };
 
-    const defaultRules: RegisterOptions = {
-        pattern: {
-            value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/i,
-            message: "Please provide a valid email"
-        }
-    }
-
-    return <InputField {...props} type="email" rules={defaultRules} />;
+  return (
+    <InputField
+      endContent={
+        <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+      }
+      {...props}
+      type="email"
+      rules={defaultRules}
+    />
+  );
 }
