3189875f5675ffeaa0a606f0e10e1f068e14934e
George <georgekalyvianakis18@gmail.com>
2024-10-21 23:28:13 +0000

    Add heading and description, fix unescaped character

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -15,6 +15,10 @@ export default function LoginPage(): ReactElement {
 
   return (
     <div className="container py-20 max-w-[500px] mx-auto justify-center flex-col flex items-start gap-2">
+      <div>
+        <h1 className="text-3xl tracking-wider drop-shadow-md font-extralight">Login</h1>
+        <p className="text-xs mb-2 text-slate-400">You can enter your credentials to access the application, or register a new user.</p>
+      </div>
       <FormProvider {...methods}>
         <EmailField
           name="email"
@@ -32,7 +36,7 @@ export default function LoginPage(): ReactElement {
           variant="bordered"
         />
         <div className="flex py-2 px-1 justify-between gap-2">
-          Don't have an account?
+          Don&apos;t have an account?
           <Link color="primary" href="#" size="md">
             Register here.
           </Link>
