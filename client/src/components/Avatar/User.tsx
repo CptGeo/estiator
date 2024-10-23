ef69745405281a76b1744dccfa1cd2656275ea73
George <georgekalyvianakis18@gmail.com>
2024-10-23 14:35:51 +0000

    Add dropdown trigger wrapper to fix issue

--- client/src/components/Avatar/User.tsx
+++ client/src/components/Avatar/User.tsx
@@ -1,21 +1,23 @@
-import { User } from "@nextui-org/react";
+import { DropdownTrigger, User } from "@nextui-org/react";
 import { UserData } from "../../core/types";
 
 export default function UserAvatar(props: { user: UserData }) {
   const { name, surname, username } = props.user;
 
   return (
-    <User
-      as="button"
-      avatarProps={{
-        isBordered: true,
-        name: `${name.charAt(0).toUpperCase()} ${surname
-          .charAt(0)
-          .toUpperCase()}`,
-      }}
-      className="transition-transform"
-      description={`@${username}`}
-      name={`${name} ${surname}`}
-    />
+    <DropdownTrigger>
+      <User
+        as="button"
+        avatarProps={{
+          isBordered: true,
+          name: `${name.charAt(0).toUpperCase()} ${surname
+            .charAt(0)
+            .toUpperCase()}`,
+          }}
+          className="transition-transform"
+          description={`@${username}`}
+          name={`${name} ${surname}`}
+      />
+    </DropdownTrigger>
   );
 }
