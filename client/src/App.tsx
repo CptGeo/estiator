ab774d49de14cbea04c8bd9a192bcd19ab3fdd9a
George <georgekalyvianakis18@gmail.com>
2024-10-23 00:44:10 +0000

    Add AuthLayout in router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,25 +5,37 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home/Home";
-import MainLayout from "./layouts/Main";
+import PrivateLayout from "./layouts/Private";
 import PlaygroundPage from "./pages/Playground/Playground";
 import LoginPage from "./pages/Login/Login";
+import MainLayout from "./layouts/Main";
+import AuthLayout from "./layouts/Auth";
 
 const router = createBrowserRouter([
   {
-    element: <MainLayout />,
+    element: <AuthLayout />,
     children: [
       {
-        path: "/",
-        element: <HomePage />,
+        element: <PrivateLayout />,
+        children: [
+          {
+            path: "/",
+            element: <HomePage />,
+          },
+        ],
       },
       {
-        path: "/login",
-        element: <LoginPage />
-      },
-      {
-        path: "playground",
-        element: <PlaygroundPage />,
+        element: <MainLayout />,
+        children: [
+          {
+            path: "/login",
+            element: <LoginPage />,
+          },
+          {
+            path: "playground",
+            element: <PlaygroundPage />,
+          },
+        ]
       }
     ]
   }
