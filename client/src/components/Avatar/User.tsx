1e31526ebe908daab86d12c48743e91ee4244807
George <georgekalyvianakis18@gmail.com>
2024-10-23 14:28:58 +0000

    Remove UserAvatar, implement Company and User avatar components

---
+++ client/src/components/Avatar/User.tsx
@@ -0,0 +1,21 @@
+import { User } from "@nextui-org/react";
+import { UserData } from "../../core/types";
+
+export default function UserAvatar(props: { user: UserData }) {
+  const { name, surname, username } = props.user;
+
+  return (
+    <User
+      as="button"
+      avatarProps={{
+        isBordered: true,
+        name: `${name.charAt(0).toUpperCase()} ${surname
+          .charAt(0)
+          .toUpperCase()}`,
+      }}
+      className="transition-transform"
+      description={`@${username}`}
+      name={`${name} ${surname}`}
+    />
+  );
+}
