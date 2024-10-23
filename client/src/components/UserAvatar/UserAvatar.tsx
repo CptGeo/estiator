4913d8cf74f82a7d2b119f495d9b18405a4cb184
George <georgekalyvianakis18@gmail.com>
2024-10-23 00:45:00 +0000

    Implement UserAvatar component to display when user is authenticated

---
+++ client/src/components/UserAvatar/UserAvatar.tsx
@@ -0,0 +1,46 @@
+import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, User } from "@nextui-org/react";
+import { ReactElement } from "react";
+import { useAuth, UserData } from "../../context/Authentication";
+
+type Props = {
+  user: UserData
+}
+
+export default function UserAvatar(props: Props): ReactElement {
+  const { name, surname, username } = props.user;
+  const auth = useAuth();
+
+  async function handleLogout() {
+    if (auth?.logoutAction && auth.user?.username) {
+      await auth.logoutAction(auth.user?.username);
+    }
+  }
+  
+  return (
+    <div className="flex items-center gap-4">
+      <Dropdown placement="bottom-start">
+        <DropdownTrigger>
+          <User
+            as="button"
+            avatarProps={{
+              isBordered: true,
+              name: `${name.charAt(0).toUpperCase()} ${surname.charAt(0).toUpperCase()}`
+            }}
+            className="transition-transform"
+            description={`@${username}`}
+            name={`${name} ${surname}`}
+          />
+        </DropdownTrigger>
+        <DropdownMenu aria-label="User Actions" variant="flat">
+          <DropdownItem key="profile" className="h-14 gap-2">
+            <p className="font-bold">Signed in as</p>
+            <p className="font-bold">@{username}</p>
+          </DropdownItem>
+          <DropdownItem onClick={handleLogout} key="logout" color="danger">
+            Log Out
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </div>
+  )
+}
