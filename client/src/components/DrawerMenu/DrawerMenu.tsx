19a15a301bbe32d73bc5c67d2cf9320023f26b0e
George <georgekalyvianakis18@gmail.com>
2024-10-24 12:59:44 +0100

    Make drawer fixed, fix indentation

--- client/src/components/DrawerMenu/DrawerMenu.tsx
+++ client/src/components/DrawerMenu/DrawerMenu.tsx
@@ -18,23 +18,23 @@ export default function DrawerMenu(): ReactElement {
   }
 
   return (
-    <div className={`transition-transform flex flex-col h-full absolute left-0 top-0 max-w-[300px] w-full z-50 bg-slate-800 drop-shadow-lg px-5 pt-5 pb-2 ${!open ? "translate-x-[-100%]" : ""}`}>
-    <div className="mb-5">
-      <CompanyAvatar company={ { name: "Estiator.io", description: "Company description" }} />
-    </div>
-    <div className="flex flex-col justify-between h-full">
-      <div className="flex flex-col gap-2">
-        <DrawerItem to="/" text="Home" icon={<HomeIcon />} />
-        <DrawerItem to="/playground" text="Playground" icon={<PlaygroundIcon />} />
+    <div className={`transition-transform flex flex-col h-full fixed left-0 top-0 max-w-[300px] w-full z-50 bg-slate-800 drop-shadow-lg px-5 pt-5 pb-2 ${!open ? "translate-x-[-100%]" : ""}`}>
+      <div className="mb-5">
+        <CompanyAvatar company={ { name: "Estiator.io", description: "Company description" }} />
       </div>
+      <div className="flex flex-col justify-between h-full">
+        <div className="flex flex-col gap-2">
+          <DrawerItem to="/" text="Home" icon={<HomeIcon />} />
+          <DrawerItem to="/playground" text="Playground" icon={<PlaygroundIcon />} />
+        </div>
 
-      <div className="flex flex-col gap-2">
-        <DrawerItem to="/settings" text="Settings" icon={<SettingsIcon />} />
+        <div className="flex flex-col gap-2">
+          <DrawerItem to="/settings" text="Settings" icon={<SettingsIcon />} />
+        </div>
       </div>
+      <Button onClick={() => setOpen(!open)} className="cursor-pointer absolute top-[18px] right-0 translate-x-full rounded-l-none p-0 bg-slate-700 text-slate-200 text-sm" isIconOnly size="sm" variant="solid">
+        {open ? <MenuCloseIcon /> : <MenuOpenIcon />}
+      </Button>
     </div>
-    <Button onClick={() => setOpen(!open)} className="cursor-pointer absolute top-[18px] right-0 translate-x-full rounded-l-none p-0 bg-slate-700 text-slate-200 text-sm" isIconOnly size="sm" variant="solid">
-      {open ? <MenuCloseIcon /> : <MenuOpenIcon />}
-    </Button>
-  </div>
   )
 }
