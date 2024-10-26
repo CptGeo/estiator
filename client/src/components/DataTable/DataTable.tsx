62b59a5ae22e20f690a7ebb98f570b8326a47c49
CptGeo <georgekalyvianakis18@gmail.com>
2024-10-26 23:34:34 +0200

    Implement DataTable component

--- client/src/components/DataTable/DataTable.tsx
+++ client/src/components/DataTable/DataTable.tsx
@@ -1,75 +1,60 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import React from "react";
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, DatePicker, SelectItem } from "@nextui-org/react";
-import { columns, reservations, ReservationsType } from "../../_temp_data";
+import { useState } from "react";
+import { Table, TableHeader, TableColumn, TableBody, Selection, TableRow, TableCell, User, DatePicker, Input } from "@nextui-org/react";
+import { columns, reservations } from "../../_temp_data";
+import Status from "../Status/Status";
 import DataActions from "./DataActions";
-import { CalendarDate } from "@internationalized/date";
-import DateTimeField from "../Fields/DateTime";
-import SelectField from "../Fields/Select";
-import { FormProvider, useForm } from "react-hook-form";
+import { parseDate } from "@internationalized/date";
 
 export default function DataTable() {
-  const methods = useForm();
-
-  const _renderCell = React.useCallback((reservation: ReservationsType, columnKey: any) => {
-    const cellValue = reservations[columnKey];
-
-    const reservationDate = reservation.date.split("-").map( (val)  => parseInt(val) );
-    const calendarDate = new CalendarDate(reservationDate[0], reservationDate[1], reservationDate[3]);
-
-    switch (columnKey) {
-      case "name":
-        return (
-          <User
-            avatarProps={{ radius: "lg", name: "GK" }}
-            description={reservation.user.email}
-            name={"name"}
-          >
-            {reservation.user.email}
-          </User>
-        );
-      case "date":
-        return (
-          <div className="flex flex-col">
-            <DatePicker
-              className="max-w-xs"
-              granularity="day"
-              defaultValue={calendarDate}
-            />
-          </div>
-        );
-      case "actions":
-        return <DataActions />;
-      default:
-        return cellValue;
-    }
-  }, []);
+  const [selectedKeys, setSelectedKeys] = useState<Selection>(new Set());
 
   return (
-  <FormProvider {...methods}>
-    <Table aria-label="Example table with custom cells">
-        <TableHeader columns={columns}>
-          {(column) => (
-            <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
-              {column.name}
-            </TableColumn>
-          )}
-        </TableHeader>
-        <TableBody items={reservations}>
-          <TableRow>
-            <TableCell><div>George Kalyvianakis</div></TableCell>
-            <TableCell><div><DateTimeField/></div></TableCell>
-            <TableCell><div><SelectField defaultSelectedKeys={["0"]} name="time"><SelectItem key={0}>02:00pm</SelectItem><SelectItem key={1}>01:30pm</SelectItem></SelectField></div></TableCell>
-            <TableCell><div><SelectField defaultSelectedKeys={["0"]} name="table"><SelectItem key={0}>34</SelectItem><SelectItem key={1}>12</SelectItem></SelectField></div></TableCell>
-            <TableCell><div><DataActions/></div></TableCell>
+    <Table
+      aria-label="Example table with custom cells"
+      selectionMode="multiple"
+      selectedKeys={selectedKeys}
+      onSelectionChange={setSelectedKeys}
+    >
+      <TableHeader columns={columns}>
+        {(column) => (
+          <TableColumn
+            key={column.uid}
+            align={column.uid === "actions" ? "center" : "start"}
+          >
+            {column.name}
+          </TableColumn>
+        )}
+      </TableHeader>
+      <TableBody items={reservations}>
+        {(reservation) => {
+          const date = parseDate(reservation.date);
+          return (
+            <TableRow>
+              <TableCell>
+                  <User
+                  avatarProps={{ radius: "full", size: "sm" }}
+                  classNames={{
+                      description: "text-default-500",
+                  }}
+                  description={reservation.user.email || reservation.user.phone}
+                  name={`${reservation.user.name} ${reservation.user.surname}`}
+              />
+              </TableCell>
+              <TableCell>
+                  <DatePicker
+                      isReadOnly
+                      className="max-w-xs"
+                      granularity="day"
+                      value={date} />
+              </TableCell>
+              <TableCell><Input value={reservation.time} isReadOnly /></TableCell>
+              <TableCell><Input value={reservation.table.toString()} isReadOnly /></TableCell>
+              <TableCell><Input value={reservation.persons.toString()} isReadOnly /></TableCell>
+              <TableCell><Status status={reservation.status} /></TableCell>
+              <TableCell><div><DataActions /></div></TableCell>
           </TableRow>
-          {/* {(item: ReservationsType) => (
-            <TableRow key={item.id}>
-            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
-            </TableRow>
-            )} */}
-        </TableBody>
-      </Table>
-    </FormProvider>
+          )}}
+      </TableBody>
+    </Table>
   );
 }
