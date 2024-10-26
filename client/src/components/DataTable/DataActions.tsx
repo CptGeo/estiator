ebfaa7a5b83e0ba7439e39747cc1b155b3cd44b8
CptGeo <georgekalyvianakis18@gmail.com>
2024-10-26 23:31:52 +0200

    Remove redundant import

--- client/src/components/DataTable/DataActions.tsx
+++ client/src/components/DataTable/DataActions.tsx
@@ -1,4 +1,4 @@
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button, DropdownSection, Divider } from "@nextui-org/react";
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button, DropdownSection } from "@nextui-org/react";
 import MenuDotsIcon from "../Icons/MenuDotsIcon";
 
 export default function DataActions() {
