e9ab9b95bd2f931d4cc15b03491d97229a3fa53c
CptGeo <georgekalyvianakis18@gmail.com>
2024-10-27 21:05:24 +0100

    Make user inputs readonly, render spinner on loading, render message when no rows

--- client/src/components/Reservations/Table.tsx
+++ client/src/components/Reservations/Table.tsx
@@ -1,13 +1,26 @@
 import { useCallback } from "react";
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, DatePicker, Input } from "@nextui-org/react";
-import { columns, reservations } from "../../_temp_data";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, DatePicker, Input, Spinner } from "@nextui-org/react";
+// import { reservations } from "../../_temp_data";
 import Status from "../Status/Status";
 import { parseDate, parseTime } from "@internationalized/date";
 import { ReservationData } from "../../core/types";
 import ReservationsActions from "./Actions";
 import { getFullName } from "../../core/utils";
+import useGetReservations from "../../hooks/useGetReservations";
+
+const columns = [
+  { name: "NAME", uid: "name" },
+  { name: "DATE", uid: "date" },
+  { name: "TIME", uid: "time" },
+  { name: "TABLE", uid: "table" },
+  { name: "PERSONS", uid: "persons" },
+  { name: "STATUS", uid: "status" },
+  { name: "ACTIONS", uid: "actions" },
+];
 
 export default function ReservationsTable() {
+  const { data: reservations } = useGetReservations();
+
   const renderRow = useCallback((reservation: ReservationData) => {
     const date = parseDate(reservation.date);
     const time = parseTime(reservation.time);
@@ -16,29 +29,30 @@ export default function ReservationsTable() {
       <TableRow>
         <TableCell>
             <User
-            avatarProps={{ radius: "full", size: "sm" }}
-            classNames={{
-                description: "text-default-500",
-            }}
-            description={reservation.user.email || reservation.user.phone}
-            name={getFullName(reservation.user)}
+              avatarProps={{ radius: "full", size: "sm" }}
+              classNames={{
+                  description: "text-default-500",
+              }}
+              description={reservation.user.email || reservation.user.phone}
+              name={getFullName(reservation.user)}
         />
         </TableCell>
         <TableCell>
             <DatePicker
-                isReadOnly
-                className="max-w-xs"
-                granularity="day"
-                value={date} />
+              isReadOnly
+              aria-label="Date"
+              className="max-w-xs"
+              granularity="day"
+              value={date} />
         </TableCell>
-        <TableCell><Input value={time.toString().slice(0, -3)} isReadOnly /></TableCell>
-        <TableCell><Input value={reservation.table.toString()} isReadOnly /></TableCell>
-        <TableCell><Input value={reservation.persons.toString()} isReadOnly /></TableCell>
+        <TableCell><Input value={time.toString().slice(0, -3)} aria-label="Time" isReadOnly /></TableCell>
+        <TableCell><Input value={reservation.table.toString()} aria-label="Table" isReadOnly /></TableCell>
+        <TableCell><Input value={reservation.persons.toString()} aria-label="Persons" isReadOnly /></TableCell>
         <TableCell><Status status={reservation.status} /></TableCell>
         <TableCell><div><ReservationsActions reservation={reservation} /></div></TableCell>
     </TableRow>
     )
-  }, []);
+  }, [reservations]);
 
   return (
     <Table aria-label="Example table with custom cells">
@@ -52,7 +66,12 @@ export default function ReservationsTable() {
           </TableColumn>
         )}
       </TableHeader>
-      <TableBody items={reservations}>
+      <TableBody
+          emptyContent={<p>Currently, there are no reservations</p>}
+          isLoading={!Array.isArray(reservations)}
+          items={reservations || []}
+          loadingContent={<Spinner label="Loading..." />}
+      >
         {(reservation) => renderRow(reservation)}
       </TableBody>
     </Table>
