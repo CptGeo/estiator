ee2467742621ad90644092d97c94115790b2c06f
CptGeo <georgekalyvianakis18@gmail.com>
2024-10-27 21:03:21 +0100

    Implement CancelReservation modal

---
+++ client/src/components/Modal/CancelReservation.tsx
@@ -0,0 +1,50 @@
+import { useDisclosure } from "@nextui-org/react";
+import ConfirmationModal from "./Confirmation";
+import { ReservationData, ReservationStatus } from "../../core/types";
+import { useState } from "react";
+import { client } from "../../core/request";
+import { getFullName } from "../../core/utils";
+
+type Props = {
+  reservation: ReservationData;
+} & ReturnType<typeof useDisclosure>;
+
+export default function CancelReservationModal(props: Props) {
+  const { reservation, ...disclosureProps } = props;
+  const [loading, setLoading] = useState(false);
+
+  async function handleAction() {
+    const data: ReservationData = {
+      ...reservation,
+      status: ReservationStatus.CANCELLED,
+    };
+    try {
+      setLoading(true);
+      await client.put(`/reservation/${reservation.id}`, { ...data });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+      disclosureProps.onClose();
+    }
+  }
+
+  return (
+    <ConfirmationModal
+      {...disclosureProps}
+      title="Cancel reservation"
+      cancelText="Abort"
+      confirmText="Cancel reservation"
+      confirmButtonProps={{ color: "danger", isLoading: loading }}
+      body={
+        <p>
+          The reservation of customer{" "}
+          <strong>{getFullName(reservation.user)}</strong> will be cancelled.{" "}
+          <br />
+          Are you sure you want to continue?
+        </p>
+      }
+      callback={handleAction}
+    />
+  );
+}
