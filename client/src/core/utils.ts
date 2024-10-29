eaf5b0d623fb983300cec21766638a3f63637f8f
CptGeo <georgekalyvianakis18@gmail.com>
2024-10-29 09:44:43 +0100

    Implement function to improve error handling

--- client/src/core/utils.ts
+++ client/src/core/utils.ts
@@ -30,3 +30,22 @@ export function sleep(ms: number): Promise<number> {
 export function getFullName(user: UserData): string {
     return `${user.name} ${user.surname}`;
 }
+
+/**
+ * Ensures that the thrown value is indeed an Error. Converts it to Error if it is not.
+ */
+export function ensureErr(value: unknown): Error {
+    if (value instanceof Error) {
+        return value;
+    }
+
+    let stringified = '[Unable to stringify the thrown value]';
+    try {
+        stringified = JSON.stringify(value);
+    } catch {
+        // no need to handle catch; use default stringified message
+    }
+
+    const error = new Error(`This value was thrown as is, not through an Error: ${stringified}`);
+    return error;
+}
