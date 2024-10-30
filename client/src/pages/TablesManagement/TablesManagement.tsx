e9886b82919562585bf7d806e76c7f657938af6a
CptGeo <georgekalyvianakis18@gmail.com>
2024-10-30 15:15:52 +0100

    Add mock coordinates and render GridDraggable

--- client/src/pages/TablesManagement/TablesManagement.tsx
+++ client/src/pages/TablesManagement/TablesManagement.tsx
@@ -1,7 +1,8 @@
 import { ReactElement, useMemo, useState } from "react";
 import PageHeader from "../../components/PageHeader/PageHeader";
 import { createSnapModifier, restrictToFirstScrollableAncestor } from "@dnd-kit/modifiers";
-import { DraggableStory } from "../../components/DragNDrop/DraggableStory";
+import GridWrapper from "../../components/Grid/GridWrapper";
+import { DefaultCoordinates, GridDraggable } from "../../components/Grid/GridDraggable";
 
 export default function TablesManagementPage(): ReactElement {
 
@@ -11,11 +12,9 @@ export default function TablesManagementPage(): ReactElement {
         heading="Tables management"
         subheading="Here you can manage your restaurant tables and layout"
       />
-      <div className="grid-outer-bg w-full overflow-auto max-h-[400px] md:max-h-[650px]"> {/* !important rules because grid background needs a wrapper for the pattern to remain fixed to position */}
-        <div className="relative h-[1500px] w-[1500px] overflow-hidden grid-bg z-0 justify-between bg-content2 rounded-large shadow-small">
-          <Example />
-        </div>
-      </div>
+      <GridWrapper>
+        <Example />
+      </GridWrapper>
     </>
   );
 }
@@ -41,177 +40,39 @@ const Example = () => {
     tolerance: 5
   };
 
+  /** Mock coordinates */
+  const defaultCoordinates: DefaultCoordinates = {
+    "A1": { x: 200, y: 480 },
+    "A2": { x: 200, y: 360 },
+    "A3": { x: 200, y: 240 },
+    "A4": { x: 200, y: 120 },
+    "B1": { x: 360, y: 480 },
+    "B2": { x: 360, y: 360 },
+    "B3": { x: 360, y: 240 },
+    "B4": { x: 360, y: 120 },
+    "C1": { x: 520, y: 480 },
+    "C2": { x: 520, y: 360 },
+    "C3": { x: 520, y: 240 },
+    "D1": { x: 680, y: 480 },
+    "D2": { x: 680, y: 360 },
+    "D3": { x: 680, y: 240 },
+    "Z1": { x: 1160, y: 120 },
+    "Z2": { x: 1040, y: 220 },
+    "Z3": { x: 1040, y: 120 },
+    "Z4": { x: 1160, y: 220 },
+  }
+
   return (
     <>
-      <DraggableStory
+      <GridDraggable
         activationConstraint={activationConstraint}
         modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
         buttonStyle={buttonStyle}
         buttonClassName={"!bg-success text-white"}
-        defaultCoordinates={{ x: 200, y: 480 }}
         label={"A1"}
         style={style}
         key={"table-A1"}
-        />
-      <DraggableStory
-        activationConstraint={activationConstraint}
-        modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
-        buttonStyle={buttonStyle}
-        buttonClassName={"!bg-success text-white"}
-        defaultCoordinates={{ x: 200, y: 280 }}
-        label={"A2"}
-        style={style}
-        key={"table-A2"}
-        />
-
-      <DraggableStory
-        activationConstraint={activationConstraint}
-        modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
-        buttonStyle={buttonStyle}
-        buttonClassName={"!bg-success text-white"}
-        defaultCoordinates={{ x: 200, y: 80 }}
-        label={"A3"}
-        style={style}
-        key={"table-A3"}
-        />
-
-      <DraggableStory
-        activationConstraint={activationConstraint}
-        modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
-        buttonStyle={buttonStyle}
-        buttonClassName={"!bg-success text-white"}
-        defaultCoordinates={{ x: 200, y: -120 }}
-        label={"A4"}
-        style={style}
-        key={"table-A4"}
-      />
-
-      <DraggableStory
-        activationConstraint={activationConstraint}
-        modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
-        buttonStyle={buttonStyle}
-        buttonClassName={"!bg-danger text-white"}
-        defaultCoordinates={{ x: 360, y: 160 }}
-        label={"B1"}
-        style={style}
-        key={"table-B1"}
-        />
-      <DraggableStory
-        activationConstraint={activationConstraint}
-        modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
-        buttonStyle={buttonStyle}
-        buttonClassName={"!bg-danger text-white"}
-        defaultCoordinates={{ x: 360, y: -40 }}
-        label={"B2"}
-        style={style}
-        key={"table-B2"}
-        />
-
-      <DraggableStory
-        activationConstraint={activationConstraint}
-        modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
-        buttonStyle={buttonStyle}
-        buttonClassName={"!bg-danger text-white"}
-        defaultCoordinates={{ x: 360, y: -240 }}
-        label={"B3"}
-        style={style}
-        key={"table-B3"}
-      />
-
-      <DraggableStory
-        activationConstraint={activationConstraint}
-        modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
-        buttonStyle={buttonStyle}
-        buttonClassName={"!bg-danger text-white"}
-        defaultCoordinates={{ x: 360, y: -440 }}
-        label={"B4"}
-        style={style}
-        key={"table-B4"}
-      />
-
-    <DraggableStory
-        activationConstraint={activationConstraint}
-        modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
-        buttonStyle={buttonStyle}
-        buttonClassName={"!bg-warning text-white"}
-        defaultCoordinates={{ x: 520, y: -160 }}
-        label={"C1"}
-        style={style}
-        key={"table-C1"}
-        />
-      <DraggableStory
-        activationConstraint={activationConstraint}
-        modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
-        buttonStyle={buttonStyle}
-        buttonClassName={"!bg-warning text-white"}
-        defaultCoordinates={{ x: 520, y: -360 }}
-        label={"C2"}
-        style={style}
-        key={"table-C2"}
-        />
-
-      <DraggableStory
-        activationConstraint={activationConstraint}
-        modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
-        buttonStyle={buttonStyle}
-        buttonClassName={"!bg-warning text-white"}
-        defaultCoordinates={{ x: 520, y: -560 }}
-        label={"C3"}
-        style={style}
-        key={"table-C3"}
-      />
-      <DraggableStory
-        activationConstraint={activationConstraint}
-        modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
-        buttonStyle={buttonStyle}
-        buttonClassName={"!bg-secondary text-white"}
-        defaultCoordinates={{ x: 680, y: -400 }}
-        label={"D1"}
-        style={style}
-        key={"table-D1"}
-        />
-      <DraggableStory
-        activationConstraint={activationConstraint}
-        modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
-        buttonStyle={buttonStyle}
-        buttonClassName={"!bg-secondary text-white"}
-        defaultCoordinates={{ x: 680, y: -600 }}
-        label={"D2"}
-        style={style}
-        key={"table-D2"}
-        />
-
-      <DraggableStory
-        activationConstraint={activationConstraint}
-        modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
-        buttonStyle={buttonStyle}
-        buttonClassName={"!bg-secondary text-white"}
-        defaultCoordinates={{ x: 680, y: -800 }}
-        label={"D3"}
-        style={style}
-        key={"table-D3"}
-      />
-
-      <DraggableStory
-        activationConstraint={activationConstraint}
-        modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
-        buttonStyle={buttonStyle}
-        buttonClassName={"!bg-success text-white"}
-        defaultCoordinates={{ x: 1040, y: -1040 }}
-        label={"Πίσω 1"}
-        style={style}
-        key={"table-back-1"}
-      />
-
-      <DraggableStory
-        activationConstraint={activationConstraint}
-        modifiers={[snapToGrid, restrictToFirstScrollableAncestor]}
-        buttonStyle={buttonStyle}
-        buttonClassName={"!bg-success text-white"}
-        defaultCoordinates={{ x: 1040, y: -1020 }}
-        label={"Πίσω 2"}
-        style={style}
-        key={"table-back-2"}
+        defaultCoordinates={defaultCoordinates}
       />
     </>
   );
