3ef8e07eff7f58c7b3276a507f2160d7833640b5
CptGeo <georgekalyvianakis18@gmail.com>
2024-11-03 12:56:08 +0100

    Implement topContent for tables management

--- client/src/components/TablesGrid/TablesGrid.tsx
+++ client/src/components/TablesGrid/TablesGrid.tsx
@@ -3,8 +3,9 @@ import { GridDndContext } from "../Grid/GridDndContext";
 import { CSSProperties, ReactElement, useCallback, useMemo } from "react";
 import useGetTables from "../../hooks/useGetTables";
 import { isUndefined, normalize } from "../../core/utils";
-import { Spinner } from "@nextui-org/react";
+import { Button, Spinner } from "@nextui-org/react";
 import { TableData } from "../../core/types";
+import AddIcon from "../Icons/AddIcon";
 
 type Props = {
   size: number;
@@ -34,7 +35,20 @@ export default function TablesGrid(props: Props): ReactElement {
   const tables = useGetTables();
   const getNormalizedTable = useCallback((data: TableData[] | null | undefined) => normalize<TableData>(data), [tables]);
 
+  const tablesCount = tables?.length;
+
+  function topContent(): ReactElement {
+    return (
+      <div className="flex flex-row justify-between items-end">
+        <p className="text-xs text-default-600">{tablesCount! > 0 && `Total ${tablesCount} table${tablesCount! > 1 ? "s" : ""}`}</p>
+        <Button color="primary"><AddIcon className="text-md" />Add table</Button>
+      </div>
+    )
+  }
+
   return !isUndefined(tables) ? (
+    <div className="flex flex-col gap-4">
+      {topContent()}
       <div className="grid-outer-bg rounded-large w-full overflow-auto max-h-[400px] md:max-h-[650px]">
         <div style={{ "--grid-size": `${gridSize}px` } as CSSProperties} className="relative h-[1500px] w-[1500px] overflow-hidden grid-bg z-0 justify-between bg-content2 shadow-inner">
           <GridDndContext
@@ -46,7 +60,8 @@ export default function TablesGrid(props: Props): ReactElement {
             gridSize={gridSize}
           />
         </div>
+      </div>
     </div>
 
-  ) : <div className="p-4"><Spinner /></div>;
+  ) : <div className="p-4 w-full flex justify-center"><Spinner /></div>;
 }
