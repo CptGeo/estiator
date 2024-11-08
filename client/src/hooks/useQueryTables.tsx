c8a972bd1732a067a2976e7c38571d98738459dd
CptGeo <georgekalyvianakis18@gmail.com>
2024-11-08 15:43:00 +0100

    Stop refetching after encountering error

--- client/src/hooks/useQueryTables.tsx
+++ client/src/hooks/useQueryTables.tsx
@@ -4,11 +4,11 @@ import { HttpStatusCode } from "axios";
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
 
 export default function useQueryTables(interval?: number): UseQueryResult<TableData[] | undefined> {
-
   const query = useQuery({ 
     queryKey: ['tables'], 
     queryFn: getTables,
-    refetchInterval: interval
+    // stop refetching after encountering error
+    refetchInterval: ({state}) => state.fetchFailureCount > 0 ? false : interval
   });
 
   async function getTables<T extends TableData[]>(): Promise<T | undefined> {
