bace2e3800db03d6ef6252c4050ce521ffc9c985
CptGeo <georgekalyvianakis18@gmail.com>
2024-11-12 00:55:28 +0100

    Add mock Reservation widget to Dashboard

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,22 @@
 import type { ReactElement } from "react";
 import PageHeader from "@components/PageHeader/PageHeader";
+import ReservationWidget from "@components/Widget/Reservation";
 
 export default function DashboardPage(): ReactElement {
-  return <PageHeader heading="Dashboard" subheading="Nothing here yet..." />
-};
+  return (
+    <>
+      <PageHeader
+        heading="Dashboard"
+        subheading="Here you can manage existing or create new reservations for your business."
+      />
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="grid gap-4">
+          <div className="h-auto max-w-full rounded-lg">
+            {/* Render widget here */}
+            <ReservationWidget />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
