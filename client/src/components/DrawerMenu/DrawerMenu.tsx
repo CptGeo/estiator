df876e9553345c11d523ce4f75693e66392af465
George <georgekalyvianakis18@gmail.com>
2024-12-15 20:26:15 +0200

    Implement helper to check if menu item is currently active

--- client/src/components/DrawerMenu/DrawerMenu.tsx
+++ client/src/components/DrawerMenu/DrawerMenu.tsx
@@ -2,6 +2,7 @@ import { Button } from "@nextui-org/react";
 import type { ReactElement } from "react";
 import MenuCloseIcon from "@components/Icons/MenuCloseIcon";
 import MenuOpenIcon from "@components/Icons/MenuOpenIcon";
+import type { Location, To } from "react-router-dom";
 import { useLocation, useNavigate } from "react-router-dom";
 import SettingsIcon from "@components/Icons/SettingsIcon";
 import CompanyAvatar from "@components/Avatar/Company";
@@ -19,12 +20,24 @@ export default function DrawerMenu(): ReactElement {
 
   function DrawerItem(props:  { to: string, icon: ReactElement, text: string } ): ReactElement {
     const { to, icon, text } = props;
-    const current = location.pathname == to;
+
+    function isCurrent(location: Location, to: To) {
+      const tokens = location.pathname.split("/").splice(1);
+
+      /** TODO: Needs to be improved. Returns `true` when at least one url token matches the current */
+      for(const token of tokens) {
+        if (token === to.toString().slice(1)) {
+          return true;
+        }
+      }
+      return false;
+    }
+
     return (
       <Button
         onClick={() => navigate(to)}
         variant="light"
-        className={`justify-start text-slate-300 ${current ? "bg-slate-600" : ""}`}
+        className={`justify-start text-slate-300 ${isCurrent(location, to) ? "bg-slate-600" : ""}`}
         startContent={icon}>
         {text}
       </Button>
