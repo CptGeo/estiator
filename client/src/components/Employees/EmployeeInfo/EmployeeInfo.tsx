e3e1ca0148dfd8a9a580f09d4360a61d583d9c5f
George <georgekalyvianakis18@gmail.com>
2024-12-15 22:45:49 +0200

    Implement EmployeeSchedule

--- client/src/components/Employees/EmployeeInfo/EmployeeInfo.tsx
+++ client/src/components/Employees/EmployeeInfo/EmployeeInfo.tsx
@@ -8,12 +8,14 @@ import ConfirmationModal from "@components/Modal/Confirmation";
 import Status from "@components/Status/Employee/Status";
 import { EmployeeStatus, EmployeeStatuses, Role, Roles, type EmployeeData } from "@core/types";
 import { deleteReq, patchReq } from "@core/utils";
+import { Time } from "@internationalized/date";
 import { SelectItem, Button, Image, useDisclosure } from "@nextui-org/react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useEffect, type ReactElement } from "react";
 import type { FieldValues } from "react-hook-form";
 import { FormProvider, useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
+import EmployeeSchedule from "./EmployeeSchedule";
 
 export default function EmployeeInfo(props: {
   employee: EmployeeData;
@@ -31,7 +33,7 @@ export default function EmployeeInfo(props: {
   const { isPending: deletePending, mutateAsync: deleteItem } = useMutation({
     mutationFn: (id: unknown) => deleteReq(`employees/${id}`),
     onSettled: () => {
-      queryClient.refetchQueries( { queryKey: ["employees"] } )
+      queryClient.refetchQueries({ queryKey: ["employees"] })
     }
   });
 
@@ -43,7 +45,30 @@ export default function EmployeeInfo(props: {
     role: employee?.role,
     position: employee?.position,
     status: employee?.status,
-    tables: employee?.tables
+    tables: employee?.tables,
+
+    // temp static data
+    monOff: false,
+    tueOff: false,
+    wedOff: false,
+    thuOff: false,
+    friOff: false,
+    satOff: true,
+    sunOff: true,
+    monCheckin: new Time(15, 30),
+    monCheckout: new Time(15, 30),
+    tueCheckin: new Time(15, 30),
+    tueCheckout: new Time(15, 30),
+    wedCheckin: new Time(15, 30),
+    wedCheckout: new Time(15, 30),
+    thuCheckin: new Time(15, 30),
+    thuCheckout: new Time(15, 30),
+    friCheckin: new Time(15, 30),
+    friCheckout: new Time(15, 30),
+    satCheckin: new Time(15, 30),
+    satCheckout: new Time(15, 30),
+    sunCheckin: new Time(15, 30),
+    sunCheckout: new Time(15, 30)
   }
 
   const methods = useForm({
@@ -61,8 +86,8 @@ export default function EmployeeInfo(props: {
       id: employee.id
     } as EmployeeData);
 
-    queryClient.invalidateQueries( { queryKey: [`employees/${employee.id}`] } );
-    queryClient.refetchQueries( { queryKey: ["employees"] } );
+    queryClient.invalidateQueries({ queryKey: [`employees/${employee.id}`] });
+    queryClient.refetchQueries({ queryKey: ["employees"] });
   }
 
   async function handleDelete(id: unknown) {
@@ -78,7 +103,7 @@ export default function EmployeeInfo(props: {
   return (
     <FormProvider {...methods}>
       <form onSubmit={methods.handleSubmit(handleSubmit)} noValidate>
-        <div className="flex flex-col gap-7">
+        <div className="flex flex-col gap-7 mb-3">
           <div className="flex flex-row items-center justify-between">
             <div className="flex flex-row items-center gap-2">
               <Link to="/employees-management">
@@ -147,11 +172,17 @@ export default function EmployeeInfo(props: {
               </div>
             </div>
             <div className="w-full lg:w-1/4 p-1">
-              <h3 className="opacity-65 uppercase text-sm py-3">Role & Responsibilities</h3>
+              <h3 className="opacity-65 uppercase text-sm py-3">
+                Role & Responsibilities
+              </h3>
               <div className="flex flex-col gap-2">
                 <SelectField name="role" label="Role">
                   {Object.values(Role).map((item: Role) => {
-                    return <SelectItem key={item} value={item}>{Roles[item]}</SelectItem>
+                    return (
+                      <SelectItem key={item} value={item}>
+                        {Roles[item]}
+                      </SelectItem>
+                    );
                   })}
                 </SelectField>
                 <InputField
@@ -160,37 +191,52 @@ export default function EmployeeInfo(props: {
                   isRequired
                   maxLength={50}
                 />
-                <TablesSelect name="tables" label="Assigned Tables" selectionMode="multiple" />
+                <TablesSelect
+                  name="tables"
+                  label="Assigned Tables"
+                  selectionMode="multiple"
+                />
                 <h3 className="opacity-65 uppercase text-sm py-3">Status</h3>
                 <SelectField name="status">
                   {Object.values(EmployeeStatus).map((item: EmployeeStatus) => {
-                    return <SelectItem key={item} value={item}>{EmployeeStatuses[item]}</SelectItem>
+                    return (
+                      <SelectItem key={item} value={item}>
+                        {EmployeeStatuses[item]}
+                      </SelectItem>
+                    );
                   })}
                 </SelectField>
               </div>
             </div>
           </div>
-          <div className="w-full flex gap-3">
-            <Button
-              variant="solid"
-              className="max-w-[350px] w-full text-md py-6"
-              color="primary"
-              type="submit"
-              isLoading={savePending}
-              isDisabled={
-                !methods.formState.isDirty || !methods.formState.isValid
-              }
-            >
-              Save changes
-            </Button>
-          </div>
+        </div>
+
+        <EmployeeSchedule />
+
+        <div className="w-full flex gap-3">
+          <Button
+            variant="solid"
+            className="max-w-[350px] w-full text-md py-6"
+            color="primary"
+            type="submit"
+            isLoading={savePending}
+            isDisabled={
+              !methods.formState.isDirty || !methods.formState.isValid
+            }
+          >
+            Save changes
+          </Button>
         </div>
       </form>
       <ConfirmationModal
         {...modal}
         body={
           <p>
-            Employee <strong>{employee.name} {employee.surname}</strong> will be <strong>removed.</strong>
+            Employee{" "}
+            <strong>
+              {employee.name} {employee.surname}
+            </strong>{" "}
+            will be <strong>removed.</strong>
           </p>
         }
         title={`Remove employee ${employee.name} ${employee.surname}`}
