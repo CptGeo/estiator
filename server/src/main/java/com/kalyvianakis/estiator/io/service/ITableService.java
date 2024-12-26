e897286ca559fc0a9e54e71b54c62b7842bc692e
George <georgekalyvianakis18@gmail.com>
2024-12-26 03:23:15 +0200

    Remove redundant imports

--- server/src/main/java/com/kalyvianakis/estiator/io/service/ITableService.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/service/ITableService.java
@@ -3,7 +3,6 @@ package com.kalyvianakis.estiator.io.service;
 import com.kalyvianakis.estiator.io.model.Table;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ITableService {
     public Table save(Table table);
