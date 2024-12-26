28bc28d7d321a2c9eaf86fc8f7172b342486f136
George <georgekalyvianakis18@gmail.com>
2024-12-26 03:22:23 +0200

    Restructure table model, add validation annotations

--- server/src/main/java/com/kalyvianakis/estiator/io/model/Table.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/model/Table.java
@@ -1,37 +1,75 @@
 package com.kalyvianakis.estiator.io.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
-@jakarta.persistence.Table(name = "restaurant-table")
+@jakarta.persistence.Table(name = "tables")
 public class Table {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String name;
-    private int capacity;
+    private Integer id;
 
-    public int getId() {
+    @NotBlank(message = "Value for 'label' cannot be blank")
+    private String label;
+
+    @NotNull(message = "Value for 'capacity' cannot be empty")
+    private Integer capacity;
+
+    @NotNull(message = "Value for 'x' cannot be empty")
+    private Integer x;
+
+    @NotNull(message = "Value for 'y' cannot be empty")
+    private Integer y;
+
+    private String color;
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCapacity() {
+    public Integer getCapacity() {
         return capacity;
     }
 
-    public void setCapacity(int capacity) {
+    public void setCapacity(Integer capacity) {
         this.capacity = capacity;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+    
+    public void setLabel(String label) {
+        this.label = label;
     }
 }
