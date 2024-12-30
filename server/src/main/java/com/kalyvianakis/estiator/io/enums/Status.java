f98ec6a323e1caeca824d3f76683f02e553bddb1
George <georgekalyvianakis18@gmail.com>
2024-12-30 02:02:49 +0200

    Convert Status enum values to short, add static of method

--- server/src/main/java/com/kalyvianakis/estiator/io/enums/Status.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/enums/Status.java
@@ -1,5 +1,34 @@
 package com.kalyvianakis.estiator.io.enums;
 
+import java.util.stream.Stream;
+
 public enum Status {
-  Pending, Cancelled, Confirmed, Completed
+  Pending((short) 0), 
+  Cancelled((short) 1),
+  Confirmed((short) 2),
+  Completed((short) 3);
+
+  private Short label;
+
+  Status(Short label) {
+    this.label = label;
+  }
+
+  public Short getLabel() {
+    return label;
+  }
+
+  /**
+   * Converts an integer representating a status to its corresponding Status enum.
+   *
+   * @param status the integer representation of the status
+   * @return the corresponding Status enum
+   * @throws IllegalArgumentException if the provided status integer does not match any Status enum
+   */
+  public static Status of(Short status) throws IllegalArgumentException {
+    return Stream.of(Status.values())
+      .filter(s -> s.getLabel() == status)
+      .findFirst()
+      .orElseThrow(IllegalArgumentException::new);
+  }
 }
