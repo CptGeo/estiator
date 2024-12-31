6bf0bd55f13a29987ce8174c9622f1be3f1a44e3
George <georgekalyvianakis18@gmail.com>
2024-12-31 19:18:40 +0200

    Remove switch/case, improve status chip component

--- client/src/components/Status/Reservation/Status.tsx
+++ client/src/components/Status/Reservation/Status.tsx
@@ -1,5 +1,6 @@
 import type { ReactElement } from "react";
 import { Chip } from "@nextui-org/react";
+import type { Color } from "@core/types";
 import { ReservationStatus } from "@core/types";
 
 type Props = {
@@ -9,16 +10,12 @@ type Props = {
 export default function Status(props: Props): ReactElement {
     const { status } = props;
 
-    switch(status) {
-        case ReservationStatus.CANCELLED:
-            return <Chip color="default" variant="flat">Cancelled</Chip>;
-        case ReservationStatus.COMPLETED:
-            return <Chip color="primary" variant="flat">Completed</Chip>;
-        case ReservationStatus.PENDING:
-            return <Chip color="warning" variant="flat">Pending</Chip>;
-        case ReservationStatus.CONFIRMED:
-            return <Chip color="success" variant="flat">Confirmed</Chip>
-        default:
-            return <Chip color="danger" variant="flat">Unknown</Chip>
+    const Color: Record<ReservationStatus, Color> = {
+        [ReservationStatus.CANCELLED]: "default",
+        [ReservationStatus.COMPLETED]: "primary",
+        [ReservationStatus.PENDING]: "warning",
+        [ReservationStatus.CONFIRMED]: "success",
     }
+
+    return <Chip color={Color[status as ReservationStatus] ?? "danger"} variant="flat">{status}</Chip>
 }
