1c70357033d43de7b0a4f37d155c65586c27753f
George <georgekalyvianakis18@gmail.com>
2025-01-07 01:57:54 +0200

    Refactor TableController to throw ResourceNotFoundException for get and delete methods, simplify add method, improve error handling

--- server/src/main/java/com/kalyvianakis/estiator/io/controller/TableController.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/controller/TableController.java
@@ -1,6 +1,8 @@
 package com.kalyvianakis.estiator.io.controller;
 
 import com.kalyvianakis.estiator.io.component.patcher.TablePatcher;
+import com.kalyvianakis.estiator.io.global.ResourceNotFoundException;
+import com.kalyvianakis.estiator.io.model.MessageResponse;
 import com.kalyvianakis.estiator.io.model.Table;
 import com.kalyvianakis.estiator.io.service.TableService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,27 +24,13 @@ public class TableController {
     private TablePatcher tablePatcher;
 
     @PostMapping
-    // @todo - Fix issue with entity creation. Instead of 400 - Bad Request, I get 500 - Internal Server Error after data validation with 'false' result
-    public ResponseEntity<String> add(@RequestBody Table table) {
-        try {
-            tableService.save(table);
-            return ResponseEntity.ok().body("Table has been added successfully");
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().body("An error occurred: " + e.getMessage());
-        }
+    public ResponseEntity<Table> add(@RequestBody Table table) {
+        return ResponseEntity.ok().body(tableService.save(table));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> get(@PathVariable Integer id) {
-        try{ 
-            Table table = tableService.get(id);
-            if (table == null) {
-                return ResponseEntity.badRequest().body("Resource not found for ID: " + id);
-            }
-            return ResponseEntity.ok().body(table);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.internalServerError().body("An error occurred: " + e.getMessage());
-        }
+    public ResponseEntity<Table> get(@PathVariable Integer id) throws ResourceNotFoundException {
+        return ResponseEntity.ok().body(tableService.get(id));
     }
 
     @GetMapping()
@@ -51,38 +39,23 @@ public class TableController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable Integer id) {
-        try {
-            if(tableService.notExists(id)) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Resource not found for ID: " + id);
-            }
-            
-            tableService.delete(id);
-            return ResponseEntity.ok().body(String.format("Resource deleted for ID: ", id)); 
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.internalServerError().body("An error occurred: " + e.getMessage());
+    public ResponseEntity<?> delete(@PathVariable Integer id) throws ResourceNotFoundException {
+        if(tableService.notExists(id)) {
+            throw new ResourceNotFoundException("User not found for ID: " + id);
         }
+
+        tableService.delete(id);
+        MessageResponse response = new MessageResponse("User deleted for ID: " + id);
+        return ResponseEntity.ok().body(response);
     }
     
     @PatchMapping("/{id}")
-    public ResponseEntity<?> patch(@PathVariable Integer id, @RequestBody Table table) {
-        try {
-            if (id == null || id < 0) {
-                return ResponseEntity.badRequest().body("Invalid ID provided");
-            }
+    public ResponseEntity<?> patch(@PathVariable Integer id, @RequestBody Table table) throws ResourceNotFoundException, IllegalArgumentException {
+        Table current = tableService.get(id);
 
-            Table current = tableService.get(id);
+        tablePatcher.patch(current, table);
+        tableService.save(current);
 
-            if(current == null) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Resource not found for ID: " + id);
-            }
-
-            tablePatcher.patch(current, table); 
-            tableService.save(current);
-
-            return ResponseEntity.ok().body(current);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.internalServerError().body("An error occurred: " + e.getMessage());
-        }
+        return ResponseEntity.ok().body(current);
     }
 }
