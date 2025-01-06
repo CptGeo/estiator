96331c11378acdc8d7da3999c8d5f0ef68c446c0
George <georgekalyvianakis18@gmail.com>
2025-01-06 23:58:18 +0200

    Extend UserRepository with JpaSpecificationExecutor

--- server/src/main/java/com/kalyvianakis/estiator/io/repository/UserRepository.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.kalyvianakis.estiator.io.repository;
 
-import com.kalyvianakis.estiator.io.model.Table;
 import com.kalyvianakis.estiator.io.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {}
+public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {}
