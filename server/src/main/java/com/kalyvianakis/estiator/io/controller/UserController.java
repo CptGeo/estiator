5e730c869f83d4bcaa763dd60cf099dc787ca483
George <georgekalyvianakis18@gmail.com>
2025-01-07 00:03:13 +0200

    Remove try/catch clauses, implement get by email route, implement fetch of registered or notRegisted users

--- server/src/main/java/com/kalyvianakis/estiator/io/controller/UserController.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.kalyvianakis.estiator.io.controller;
 
 import com.kalyvianakis.estiator.io.component.patcher.UserPatcher;
-import com.kalyvianakis.estiator.io.enums.UserStatus;
+import com.kalyvianakis.estiator.io.global.ResourceNotFoundException;
+import com.kalyvianakis.estiator.io.model.MessageResponse;
 import com.kalyvianakis.estiator.io.model.User;
 import com.kalyvianakis.estiator.io.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,66 +25,56 @@ public class UserController {
     @PostMapping
     // @todo - Fix issue with entity creation. Instead of 400 - Bad Request, I get 500 - Internal Server Error after data validation with 'false' result
     public ResponseEntity<String> add(@RequestBody User user) {
-        try {
-            user.setStatusValue(user.getStatus().getLabel());
-            userService.save(user);
-            return ResponseEntity.ok().body("User has been added successfully");
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().body("An error occurred: " + e.getMessage());
-        }
+        user.setStatusValue(user.getStatus().getLabel());
+        userService.save(user);
+        return ResponseEntity.ok().body("User has been added successfully");
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> get(@PathVariable Integer id) {
-        try{ 
-            User user = userService.get(id);
-            if (user == null) {
-                return ResponseEntity.badRequest().body("Resource not found for ID: " + id);
-            }
-            return ResponseEntity.ok().body(user);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.internalServerError().body("An error occurred: " + e.getMessage());
-        }
+    public ResponseEntity<?> get(@PathVariable Integer id) throws ResourceNotFoundException {
+        return ResponseEntity.ok().body(userService.get(id));
     }
 
     @GetMapping()
-    public ResponseEntity<List<User>> get() {
+    public ResponseEntity<List<User>> get(@RequestParam(required = false, name = "registered") Boolean registered) {
+        if (registered != null) {
+            if (registered) {
+                return ResponseEntity.ok().body(userService.getRegistered());
+            } else {
+                return ResponseEntity.ok().body(userService.getNotRegistered());
+            }
+        }
+
         return ResponseEntity.ok().body(userService.get());
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable Integer id) {
-        try {
-            if(userService.notExists(id)) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Resource not found for ID: " + id);
-            }
-            
-            userService.delete(id);
-            return ResponseEntity.ok().body(String.format("Resource deleted for ID: ", id)); 
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.internalServerError().body("An error occurred: " + e.getMessage());
+    @GetMapping("/email/{email}")
+    public ResponseEntity<User> get(@PathVariable String email) throws ResourceNotFoundException {
+        User result = userService.getOneByEmail(email);
+        if (result == null) {
+            throw new ResourceNotFoundException("User not found with email: " + email);
         }
+        return ResponseEntity.ok().body(result);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Integer id) throws ResourceNotFoundException, IllegalArgumentException {
+        if(userService.notExists(id)) {
+            throw new ResourceNotFoundException("User not found for ID: " + id);
+        }
+
+        userService.delete(id);
+        MessageResponse response = new MessageResponse(String.format("Resource deleted for ID: %s", id));
+        return ResponseEntity.ok().body(response);
     }
     
     @PatchMapping("/{id}")
-    public ResponseEntity<?> patch(@PathVariable Integer id, @RequestBody User user) {
-        try {
-            if (id == null || id < 0) {
-                return ResponseEntity.badRequest().body("Invalid ID provided");
-            }
+    public ResponseEntity<?> patch(@PathVariable Integer id, @RequestBody User user) throws ResourceNotFoundException, IllegalArgumentException {
+        User current = userService.get(id);
 
-            User current = userService.get(id);
+        userPatcher.patch(current, user);
+        userService.save(current);
 
-            if(current == null) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Resource not found for ID: " + id);
-            }
-
-            userPatcher.patch(current, user);
-            userService.save(current);
-
-            return ResponseEntity.ok().body(current);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.internalServerError().body("An error occurred: " + e.getMessage());
-        }
+        return ResponseEntity.ok().body(current);
     }
 }
