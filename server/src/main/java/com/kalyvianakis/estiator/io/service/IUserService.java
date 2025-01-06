7b9de27c4cae32b182bbbb138634fed79614a0cf
George <georgekalyvianakis18@gmail.com>
2025-01-07 00:01:01 +0200

    Add getOneByEmail, getRegistered and getNotRegistered service methods

--- server/src/main/java/com/kalyvianakis/estiator/io/service/IUserService.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/service/IUserService.java
@@ -1,5 +1,6 @@
 package com.kalyvianakis.estiator.io.service;
 
+import com.kalyvianakis.estiator.io.global.ResourceNotFoundException;
 import com.kalyvianakis.estiator.io.model.User;
 
 import java.util.List;
@@ -7,7 +8,10 @@ import java.util.List;
 public interface IUserService {
     public User save(User user);
     public List<User> get();
-    public User get(int id);
+    public User get(Integer id) throws ResourceNotFoundException;
+    public User getOneByEmail(String email);
+    public List<User> getRegistered();
+    public List<User> getNotRegistered();
     public void delete(int id);
     public boolean exists(int id);
     public boolean notExists(int id);
--- server/src/main/java/com/kalyvianakis/estiator/io/service/UserService.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/service/UserService.java
@@ -1,8 +1,11 @@
 package com.kalyvianakis.estiator.io.service;
 
+import com.kalyvianakis.estiator.io.global.ResourceNotFoundException;
 import com.kalyvianakis.estiator.io.model.User;
 import com.kalyvianakis.estiator.io.repository.UserRepository;
+import com.kalyvianakis.estiator.io.specifications.UserSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,8 +27,24 @@ public class UserService implements IUserService {
     }
 
     @Override
-    public User get(int id) {
-        return userRepository.findById(id).orElse(null);
+    public User get(Integer id) throws ResourceNotFoundException {
+        return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found for ID: " + id));
+    }
+
+    @Override
+    public User getOneByEmail(String email) throws IllegalArgumentException {
+        if (email == null) {
+            throw new IllegalArgumentException("Email must not be null");
+        }
+        return userRepository.findOne(UserSpecification.emailEquals(email)).orElse(null);
+    }
+
+    public List<User> getRegistered() {
+        return userRepository.findAll(Specification.where(UserSpecification.isAdmin().or(UserSpecification.isModerator())));
+    }
+
+    public List<User> getNotRegistered() {
+        return userRepository.findAll(Specification.where(Specification.not(UserSpecification.isAdmin()).and(Specification.not(UserSpecification.isModerator()))));
     }
 
     @Override
