4024bd930c79905f88746d011203252ba25f1f6b
George <georgekalyvianakis18@gmail.com>
2025-01-09 04:27:09 +0200

    Add endpoint to retrieve user schedule by ID

--- server/src/main/java/com/kalyvianakis/estiator/io/controller/UserController.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/controller/UserController.java
@@ -6,7 +6,6 @@ import com.kalyvianakis.estiator.io.model.MessageResponse;
 import com.kalyvianakis.estiator.io.model.User;
 import com.kalyvianakis.estiator.io.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,6 +33,11 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<?> get(@PathVariable Integer id) throws ResourceNotFoundException {
         return ResponseEntity.ok().body(userService.get(id));
+    }
+
+    @GetMapping("/{id}/schedule")
+    public ResponseEntity<?> getSchedule(@PathVariable Integer id) throws ResourceNotFoundException {
+        return ResponseEntity.ok().body(userService.getSchedule(id));
     }
 
     @GetMapping()
