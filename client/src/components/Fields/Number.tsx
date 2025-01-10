663a28bd7d9a003aadc03f51a06d4d057d17db65
George <georgekalyvianakis18@gmail.com>
2025-01-10 03:33:17 +0200

    Enhance NumberField component to support min and max validation rules for numeric inputs

--- client/src/components/Fields/Number.tsx
+++ client/src/components/Fields/Number.tsx
@@ -4,13 +4,24 @@ import type { RegisterOptions } from "react-hook-form";
 import type { ControlledInputProps } from "@components/Fields/types";
 
 export default function NumberField(props: Omit<ControlledInputProps, "type">): ReactElement {
+    const { max, min, label, ...otherProps } = props;
 
     const defaultRules: RegisterOptions = {
         valueAsNumber: true,
-        validate: (value: unknown) => {
-            return Number.isNaN(value) ? "Please provide a valid numeric value": true;
+        ...max && { max: {
+            message: `${label} cannot be greater than ${max}.`,
+            value: max
+        } },
+        ...min && { min: {
+            message: `${label} cannot be less than ${min}.`,
+            value: min
+        } },
+        validate: {
+            isNumeric: (value: unknown) => {
+                return Number.isNaN(value) ? "Please provide a valid numeric value" : true;
+            }
         }
     }
 
-    return <InputField {...props} type="text" rules={defaultRules} />;
+    return <InputField {...otherProps} type="text" rules={defaultRules} />;
 }
