cc520d774564ea0a5dcf2cfd127d4ad89b46dbc6
George <georgekalyvianakis18@gmail.com>
2025-01-10 03:27:07 +0200

    Add endpoints for retrieving and adding user schedules by date

--- server/src/main/java/com/kalyvianakis/estiator/io/controller/UserController.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/controller/UserController.java
@@ -1,15 +1,28 @@
 package com.kalyvianakis.estiator.io.controller;
 
 import com.kalyvianakis.estiator.io.component.patcher.UserPatcher;
+import com.kalyvianakis.estiator.io.enums.ScheduleStatus;
 import com.kalyvianakis.estiator.io.global.ResourceNotFoundException;
 import com.kalyvianakis.estiator.io.model.MessageResponse;
+import com.kalyvianakis.estiator.io.model.Schedule;
+import com.kalyvianakis.estiator.io.model.ScheduleRequest;
 import com.kalyvianakis.estiator.io.model.User;
+import com.kalyvianakis.estiator.io.service.ScheduleService;
 import com.kalyvianakis.estiator.io.service.UserService;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Time;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.sql.Date;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @CrossOrigin
@@ -18,6 +31,9 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private ScheduleService scheduleService;
 
     @Autowired
     private UserPatcher userPatcher;
@@ -38,6 +54,61 @@ public class UserController {
     @GetMapping("/{id}/schedule")
     public ResponseEntity<?> getSchedule(@PathVariable Integer id) throws ResourceNotFoundException {
         return ResponseEntity.ok().body(userService.getSchedule(id));
+    }
+
+    @GetMapping("/{id}/schedule/{date}")
+    public ResponseEntity<Schedule> getScheduleByDate(@PathVariable Integer id, @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) throws ResourceNotFoundException {
+        return ResponseEntity.ok().body(userService.getScheduleByDate(id, date));
+    }
+
+    @PostMapping("/{id}/schedules")
+    public ResponseEntity<?> addSchedule(@PathVariable Integer id, @RequestBody ScheduleRequest scheduleRequest) throws ResourceNotFoundException {
+        LocalDate startDate = scheduleRequest.getStartDate();
+        LocalDate endDate = scheduleRequest.getEndDate();
+        Time startTime = scheduleRequest.getStartTime();
+        Time endTime = scheduleRequest.getEndTime();
+        ScheduleStatus status = scheduleRequest.getStatus();
+
+        // List of existing schedules
+        List<Schedule> existing = scheduleService.getByUserIdAndDateRange(id, startDate, endDate);
+
+        User user = userService.get(id);
+
+        // create a set for easy lookup of the whole collection
+        Set<LocalDate> existingDates = existing.stream().map(Schedule::getDate).collect(Collectors.toSet());
+
+        // update existing schedules
+        for (Schedule schedule : existing) {
+            schedule.setStartTime(startTime);
+            schedule.setEndTime(endTime);
+            schedule.setStatusValue(status.getLabel());
+            schedule.setStatus(status);
+        }
+
+        List<Schedule> newSchedules = new ArrayList<>();
+        LocalDate currentDate = scheduleRequest.getStartDate();
+
+        while (!currentDate.isAfter(endDate)) {
+            if (!existingDates.contains(currentDate)) {
+                Schedule newSchedule = new Schedule();
+                newSchedule.setDate(currentDate);
+                newSchedule.setUser(user);
+                newSchedule.setStartTime(startTime);
+                newSchedule.setEndTime(endTime);
+                newSchedule.setStatusValue(status.getLabel());
+                newSchedule.setStatus(status);
+
+                newSchedules.add(newSchedule);
+            }
+
+            currentDate = currentDate.plusDays(1);
+        }
+
+        List<Schedule> schedules = Stream.concat(
+                scheduleService.saveAll(existing).stream(),
+                scheduleService.saveAll(newSchedules).stream()).collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(schedules);
     }
 
     @GetMapping()
