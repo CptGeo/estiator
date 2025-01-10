b9af7fb3288353cd14fd60499441145cedb46796
George <georgekalyvianakis18@gmail.com>
2025-01-10 03:32:20 +0200

    Add RangeCalendarField component for integrating a range calendar with react-hook-form

---
+++ client/src/components/Fields/RangerCalendar.tsx
@@ -0,0 +1,27 @@
+import type { RangeCalendarProps } from "@nextui-org/react";
+import { RangeCalendar } from "@nextui-org/react";
+import { Controller } from "react-hook-form";
+
+type Props = {
+    name: string;
+} & Omit<RangeCalendarProps, "value" | "onChange" | "onBlur" >;
+
+export default function RangeCalendarField(props: Props) {
+    const { name, defaultValue, ...calendarProps } = props;
+
+    return (
+        <Controller
+            name={name}
+            defaultValue={defaultValue}
+            render={({ field: { onChange, onBlur, value, ref } }) => (
+                <RangeCalendar
+                    {...calendarProps}
+                    value={value}
+                    onChange={onChange}
+                    ref={ref}
+                    onBlur={onBlur}
+                />
+            )}
+        />
+    );
+}
