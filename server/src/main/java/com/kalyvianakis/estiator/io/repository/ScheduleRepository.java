bd0291546292c106ecb45c81c32e6c2e6a1f17c4
George <georgekalyvianakis18@gmail.com>
2025-01-10 03:26:55 +0200

    Add methods to ScheduleRepository and ScheduleService for date-based schedule retrieval

--- server/src/main/java/com/kalyvianakis/estiator/io/repository/ScheduleRepository.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/repository/ScheduleRepository.java
@@ -4,12 +4,22 @@ import com.kalyvianakis.estiator.io.model.Schedule;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Integer>, JpaSpecificationExecutor<Schedule>, PagingAndSortingRepository<Schedule, Integer> {
     List<Schedule> findByUserId(Integer userId, Sort sort);
+    Schedule findByUserIdAndDate(Integer userId, LocalDate date);
+
+//    @Query("SELECT * FROM schedule s WHERE (s.date BETWEEN '2025-01-01' AND '2025-05-01') AND user_id = 1")
+//    List<Schedule> findByUserIdAndDateRange(Integer userId, Date startDate, Date endDate);
+    List<Schedule> findByUserIdAndDateBetween(Integer userId, LocalDate startDate, LocalDate endDate);
+
+//    List<Schedule> saveAll
 }
--- server/src/main/java/com/kalyvianakis/estiator/io/service/ScheduleService.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/service/ScheduleService.java
@@ -1,12 +1,13 @@
 package com.kalyvianakis.estiator.io.service;
 
 import com.kalyvianakis.estiator.io.global.ResourceNotFoundException;
-import com.kalyvianakis.estiator.io.model.Reservation;
 import com.kalyvianakis.estiator.io.model.Schedule;
 import com.kalyvianakis.estiator.io.repository.ScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -16,9 +17,11 @@ public class ScheduleService implements IScheduleService {
   ScheduleRepository scheduleRepository;
 
   @Override
-  public Schedule save(Schedule reservation) {
-    return scheduleRepository.save(reservation);
+  public Schedule save(Schedule schedule) {
+    return scheduleRepository.save(schedule);
   }
+
+  public List<Schedule> saveAll(List<Schedule> schedules) { return scheduleRepository.saveAll(schedules); }
 
   @Override
   public List<Schedule> get() {
@@ -28,6 +31,10 @@ public class ScheduleService implements IScheduleService {
   @Override
   public Schedule get(Integer id) throws ResourceNotFoundException {
     return scheduleRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Schedule not found for ID: " + id));
+  }
+
+  public List<Schedule> getByUserIdAndDateRange(Integer id, LocalDate startDate, LocalDate endDate) {
+    return scheduleRepository.findByUserIdAndDateBetween(id, startDate, endDate);
   }
 
   @Override
