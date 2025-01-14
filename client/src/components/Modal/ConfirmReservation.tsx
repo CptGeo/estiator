f287ab812479340a749c3406464ce187625bd55a
George <georgekalyvianakis18@gmail.com>
2025-01-14 17:50:44 +0200

    Refactor ConfirmReservationModal to use react-query for reservation confirmation

--- client/src/components/Modal/ConfirmReservation.tsx
+++ client/src/components/Modal/ConfirmReservation.tsx
@@ -2,9 +2,9 @@ import type { useDisclosure } from "@nextui-org/react";
 import ConfirmationModal from "@components/Modal/Confirmation";
 import type { ReservationData } from "@core/types";
 import { ReservationStatus } from "@core/types";
-import { useState } from "react";
-import { client } from "@core/request";
-import { getFullName } from "@core/utils";
+import type { Key } from "react";
+import { getFullName, patchReq } from "@core/utils";
+import { useMutation } from "@tanstack/react-query";
 
 type Props = {
   reservation: ReservationData;
@@ -12,22 +12,13 @@ type Props = {
 
 export default function ConfirmReservationModal(props: Props) {
   const { reservation, ...disclosureProps } = props;
-  const [loading, setLoading] = useState(false);
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: (id: Key) => patchReq(`/reservations/${id}`, { status: ReservationStatus.CONFIRMED }),
+    onSettled: () => disclosureProps.onClose()
+  })
 
-  async function handleAction() {
-    const data: ReservationData = {
-      ...reservation,
-      status: ReservationStatus.CONFIRMED,
-    };
-    try {
-      setLoading(true);
-      await client.put(`/reservations/${reservation.id}`, { ...data });
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-      disclosureProps.onClose();
-    }
+  async function handleAction(id: Key) {
+    await mutateAsync(id);
   }
 
   return (
@@ -35,9 +26,9 @@ export default function ConfirmReservationModal(props: Props) {
       {...disclosureProps}
       title="Confirm reservation"
       confirmText="Confirm reservation"
-      confirmButtonProps={{ isLoading: loading }}
+      confirmButtonProps={{ isLoading: isPending }}
       body={<p>The reservation of customer <strong>{getFullName(reservation.createdFor)}</strong> will be confirmed for the date <strong>{reservation.date}</strong> and time <strong>{reservation.time}</strong>.<br />Are you sure you want to continue?</p>}
-      callback={handleAction}
+      callback={handleAction.bind(null, reservation.id)}
     />
   );
 }
