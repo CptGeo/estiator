e96754a60d0971dc78f0330d8fb1f2878af9f1f0
cptgeo <georgekalyvianakis18@gmail.com>
2025-01-22 22:13:59 +0100

    Refactor authentication context to include JWT decryption and improve token/user management

--- client/src/context/Authentication.tsx
+++ client/src/context/Authentication.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 import { createContext, useContext, useState } from "react"
 import { client } from "@core/request";
 import { useNavigate } from "react-router-dom";
-import { ensureErr, sleep } from "@core/utils";
+import { decryptJwt, ensureErr, sleep } from "@core/utils";
 import { AxiosError } from "axios";
 import type { AuthValue, Credentials, UserData } from "@core/types";
 import useLocalStorage from "@hooks/useLocalStorage";
@@ -10,23 +10,26 @@ import useLocalStorage from "@hooks/useLocalStorage";
 const AuthContext = createContext<AuthValue | null>(null);
 
 export function AuthProvider( props: PropsWithChildren ) {
-  const [token, setToken] = useLocalStorage<string>("token");
-  const [user, setUser] = useLocalStorage<UserData>("user");
+  const [token, setToken, removeToken] = useLocalStorage<string>("token");
+  const [user, setUser, removeUser] = useLocalStorage<UserData>("user");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   async function loginAction(credentials: Credentials): Promise<void> {
     try {
       setLoading(true);
-      await sleep(1500);
-      // perform request
       const response = await client.post("/auth/login", credentials);
 
-      setUser(response.data.data.user);
-      setToken(response.data.data.token);
+      if (response.status !== 200) {
+        throw new Error("Invalid response from server");
+      }
+
+      const claims = decryptJwt(response.data.token);
+
+      setUser(claims.user);
+      setToken(response.data.token);
 
     } catch (err) {
-      /** @todo Notify for failed login */
       let message = "";
       if (err instanceof AxiosError) {
         message = err.response ? err.response.data.message : err.message;
@@ -44,8 +47,8 @@ export function AuthProvider( props: PropsWithChildren ) {
   async function logoutAction(): Promise<void> {
     try {
       // remove all locally saved data
-      setUser(null);
-      setToken(null);
+      removeUser();
+      removeToken();
 
       // go to login page
       navigate("/login", { replace: true });
