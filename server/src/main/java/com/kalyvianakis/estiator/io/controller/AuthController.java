d2f145e1946d538c8af8ae493309a38f155ea2ad
cptgeo <georgekalyvianakis18@gmail.com>
2025-01-22 21:10:09 +0100

    Enhance login response to include AuthenticatedUser details

--- server/src/main/java/com/kalyvianakis/estiator/io/controller/AuthController.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/controller/AuthController.java
@@ -1,10 +1,14 @@
 package com.kalyvianakis.estiator.io.controller;
 
+import com.kalyvianakis.estiator.io.dto.AuthenticatedUser;
 import com.kalyvianakis.estiator.io.dto.LoginRequest;
 import com.kalyvianakis.estiator.io.dto.LoginResponse;
 import com.kalyvianakis.estiator.io.dto.SignupRequest;
+import com.kalyvianakis.estiator.io.model.User;
 import com.kalyvianakis.estiator.io.service.AuthService;
+import com.kalyvianakis.estiator.io.service.UserService;
 import com.kalyvianakis.estiator.io.utils.JwtHelper;
+import com.kalyvianakis.estiator.io.utils.ResourceNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,6 +28,9 @@ public class AuthController {
     @Autowired
     AuthenticationManager authenticationManager;
 
+    @Autowired
+    UserService userService;
+
     @PostMapping("/signup")
     public ResponseEntity<Void> signup(@Valid @RequestBody SignupRequest requestDto) throws Exception {
         authService.signup(requestDto);
@@ -31,9 +38,12 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) throws ResourceNotFoundException {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.email(), request.password()));
+        User user = userService.getOneByEmail(request.email());
+        AuthenticatedUser loggedUser = new AuthenticatedUser(user);
         String token = JwtHelper.generateToken(request.email());
-        return ResponseEntity.ok(new LoginResponse(request.email(), token));
+
+        return ResponseEntity.ok(new LoginResponse(request.email(), token, loggedUser));
     }
 }
