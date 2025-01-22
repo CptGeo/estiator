31c0a19b033449413061efbd04b98a8da461e7f8
cptgeo <georgekalyvianakis18@gmail.com>
2025-01-22 22:14:12 +0100

    Add request interceptor to include JWT token in request headers

--- client/src/core/request.ts
+++ client/src/core/request.ts
@@ -3,6 +3,23 @@ import axios from "axios";
 export const client = axios.create({
   baseURL: "http://localhost:8080",
   headers: {
-    'Accept': 'application/json'
+    'Accept': 'application/json',
   }
+});
+
+// Add a request interceptor to include the token in the request headers
+client.interceptors.request.use(config => {
+  const tokenString = localStorage.getItem("token") || "";
+
+  try {
+    if (tokenString) {
+      const token = JSON.parse(tokenString);
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  } catch (error) {
+    localStorage.removeItem("token");
+    console.error(error);
+  }
+
+  return config;
 });
