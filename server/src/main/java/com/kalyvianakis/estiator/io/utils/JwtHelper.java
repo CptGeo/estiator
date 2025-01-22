54196850ab39d2d4ec4f488a15e87fccf86e653a
cptgeo <georgekalyvianakis18@gmail.com>
2025-01-22 18:36:19 +0100

    Add JwtHelper utility class for JWT token generation and validation

---
+++ server/src/main/java/com/kalyvianakis/estiator/io/utils/JwtHelper.java
@@ -0,0 +1,59 @@
+package com.kalyvianakis.estiator.io.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.nio.file.AccessDeniedException;
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+public class JwtHelper {
+    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final Integer MINUTES = 60;
+
+    public static String generateToken(String email) {
+        Instant now = Instant.now();
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(now.plus(MINUTES, ChronoUnit.MINUTES)))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public static String extractUsername(String token) throws AccessDeniedException {
+        return getTokenBody(token).getSubject();
+    }
+
+    private static Claims getTokenBody(String token) throws AccessDeniedException {
+        try {
+            return Jwts
+                    .parser()
+                    .setSigningKey(SECRET_KEY)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+        } catch (ExpiredJwtException e) {
+            throw new AccessDeniedException("Access denied: " + e.getMessage());
+        }
+    }
+
+    public static Boolean validateToken(String token, UserDetails userDetails) throws AccessDeniedException {
+        final String username = extractUsername(token);
+        userDetails.getAuthorities();
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private static Boolean isTokenExpired(String token) throws AccessDeniedException {
+        Claims claims = getTokenBody(token);
+        return claims.getExpiration().before(new Date());
+    }
+
+}
