0187b29dd49ef85c14841946a983debf86dfb346
cptgeo <georgekalyvianakis18@gmail.com>
2025-01-22 18:34:32 +0100

    Add PropertyPrinter utility class for enhanced object string representation

---
+++ server/src/main/java/com/kalyvianakis/estiator/io/utils/PropertyPrinter.java
@@ -0,0 +1,13 @@
+package com.kalyvianakis.estiator.io.utils;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+abstract public class PropertyPrinter {
+    @Override
+    public String toString() {
+        ReflectionToStringBuilder.setDefaultStyle(ToStringStyle.MULTI_LINE_STYLE);
+        return ReflectionToStringBuilder.toString(this);
+    }
+}
+
