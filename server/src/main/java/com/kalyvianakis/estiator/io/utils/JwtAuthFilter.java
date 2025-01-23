1dbf5a36bc1094c1397e1a039662e0fe85c0ff3b
cptgeo <georgekalyvianakis18@gmail.com>
2025-01-23 12:17:49 +0100

    Update JwtAuthFilter to allow PATCH method in CORS configuration

--- server/src/main/java/com/kalyvianakis/estiator/io/utils/JwtAuthFilter.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/utils/JwtAuthFilter.java
@@ -42,7 +42,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             }
 
             response.setHeader("Access-Control-Allow-Origin", "*");
-            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
             response.setHeader("Access-Control-Max-Age", "3600");
             response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
             response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
