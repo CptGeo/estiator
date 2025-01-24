7dcc400049e2894b3c77fa13696626a571cd18cd
cptgeo <georgekalyvianakis18@gmail.com>
2025-01-24 19:23:19 +0100

    Refactor Unauthorized component layout for improved structure and styling

--- client/src/pages/Errors/Unauthorized.tsx
+++ client/src/pages/Errors/Unauthorized.tsx
@@ -4,9 +4,11 @@ import { Image } from "@nextui-org/react";
 export default function Unauthorized() {
 
     return (
-        <div className="flex items-center justify-center gap-2 mt-5">
-            <Image width={40} radius="none" src={getAssetUrl("images/unauthorized.png")} />
-            <h3 className="text-lg underline">You are unauthorized to view this page</h3>
+        <div className="flex items-center justify-center flex-col">
+            <div className="gap-2 mt-5 flex items-center justify-center">
+                <Image width={40} radius="none" src={getAssetUrl("images/unauthorized.png")} />
+                <h3 className="text-lg">You are unauthorized to view this page</h3>
+            </div>
         </div>
     )
 }
