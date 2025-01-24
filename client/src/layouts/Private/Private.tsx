b16bc0a3a50a33193420dc4f3e34cd0276f6b107
cptgeo <georgekalyvianakis18@gmail.com>
2025-01-24 19:24:49 +0100

    Enhance PrivateLayout to accept user permissions and redirect unauthorized users

--- client/src/layouts/Private/Private.tsx
+++ client/src/layouts/Private/Private.tsx
@@ -3,17 +3,21 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "@context/Authentication";
 import DrawerProvider from "@context/Drawer";
 import MainContent from "@layouts/Private/MainContent";
+import type { UserRole } from "@core/types";
+import { userIsAllowed } from "@core/auth";
 
-export default function PrivateLayout(): ReactElement {
+export default function PrivateLayout({ permissions }: { permissions: UserRole[] }): ReactElement {
   const auth = useAuth();
-
   if (!auth?.token || !auth.user) {
-    return <Navigate to="/login" state="User not authenticated" />
+    return <Navigate to="/login" state="User not authenticated" replace={false} />
   }
 
-  return (
-    <DrawerProvider>
-      <MainContent />
-    </DrawerProvider>
+  if (!userIsAllowed(auth?.user, permissions)) {
+    return <Navigate to="/unauthorized" replace={false} />
+  }
+
+  return (<DrawerProvider>
+    <MainContent />
+  </DrawerProvider>
   );
 }
