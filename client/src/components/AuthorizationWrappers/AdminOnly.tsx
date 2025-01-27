cde190d0bb75b211129685d3c0e25920816e1da3
cptgeo <georgekalyvianakis18@gmail.com>
2025-01-27 19:22:51 +0100

    Enhance AdminOnly and PermissionGuard components to support fallback rendering for unauthorized access

--- client/src/components/AuthorizationWrappers/AdminOnly.tsx
+++ client/src/components/AuthorizationWrappers/AdminOnly.tsx
@@ -2,8 +2,10 @@ import type { PropsWithChildren, ReactElement } from "react";
 import PermissionGuard from "./PermissionGuard";
 import { UserRole } from "@core/types";
 
-export default function AdminOnly(props: PropsWithChildren): ReactElement | null {
-    return <PermissionGuard permissions={[UserRole.ADMIN]}>
+type Props = PropsWithChildren<{fallback?: ReactElement}>;
+
+export default function AdminOnly(props: Props): ReactElement | null {
+    return <PermissionGuard permissions={[UserRole.ADMIN]} fallback={props.fallback}>
         {props.children}
     </PermissionGuard>
 }
--- client/src/components/AuthorizationWrappers/PermissionGuard.tsx
+++ client/src/components/AuthorizationWrappers/PermissionGuard.tsx
@@ -1,23 +1,24 @@
 import { useAuth } from "@context/Authentication";
 import { userIsAllowed } from "@core/auth";
 import type { UserRole } from "@core/types";
-import type { PropsWithChildren, ReactElement } from "react";
+import type { PropsWithChildren, ReactElement, ReactNode } from "react";
 
 type Props = PropsWithChildren<{
-    permissions: UserRole[]
+    permissions: UserRole[],
+    fallback?: ReactElement
 }>;
 
 /**
  * Wrapper component that renders element only if user has permission to view the contents
  */
-export default function PermissionGuard(props: Props): ReactElement | null {
-    const { permissions, children } = props;
+export default function PermissionGuard(props: Props): ReactNode | null {
+    const { permissions, children, fallback } = props;
     const auth = useAuth();
     const user = auth?.user;
 
     if (!user || !userIsAllowed(user, permissions)) {
-        return null;
+        return fallback ?? null;
     }
 
-    return <>{children}</>
+    return children;
 }
