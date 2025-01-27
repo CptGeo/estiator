bb7f3b79c78e4029387aa6646bf3478f58a457c1
cptgeo <georgekalyvianakis18@gmail.com>
2025-01-27 17:49:35 +0100

    Enhance JwtAuthFilter to handle JWT exceptions and set universal CORS headers

--- server/src/main/java/com/kalyvianakis/estiator/io/utils/JwtAuthFilter.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/utils/JwtAuthFilter.java
@@ -3,11 +3,13 @@ package com.kalyvianakis.estiator.io.utils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kalyvianakis.estiator.io.model.ErrorResponse;
 import com.kalyvianakis.estiator.io.service.UserService;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,13 +26,19 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private UserService userService;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private JsonHelper jsonHelper;
 
     @Autowired
     private  JwtHelper jwtHelper;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, JwtException {
+        // Set universal headers to prevent CORS issues
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Content-Type", MediaType.APPLICATION_JSON.toString());
+
         try {
             String authHeader = request.getHeader("Authorization");
             String token = null;
@@ -41,9 +49,6 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                 username = jwtHelper.extractUsername(token);
             }
 
-            response.setHeader("Access-Control-Allow-Origin", "*");
-            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
-            response.setHeader("Access-Control-Max-Age", "3600");
             response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
             response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
 
@@ -66,22 +71,18 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                     UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
                     authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    System.out.println(authenticationToken.getDetails());
                 }
             }
 
             filterChain.doFilter(request, response);
-        } catch (AccessDeniedException e) {
-            ErrorResponse errorResponse = new ErrorResponse(Integer.toString(HttpServletResponse.SC_FORBIDDEN), e.getMessage());
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            response.getWriter().write(toJson(errorResponse));
         }
-    }
+        catch (JwtException e) {
+            // token is possibly forged or modified; User is considered to be not authenticated.
+            ErrorResponse errorResponse = new ErrorResponse("It seems like you are not authenticated. Please try logging in.", e.getMessage());
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
-    private String toJson(ErrorResponse response) {
-        try {
-            return objectMapper.writeValueAsString(response);
-        } catch (Exception e) {
-            return "";
+            response.getWriter().write(JsonHelper.toJson(errorResponse));
         }
     }
 }
