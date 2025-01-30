44a07a7af1c050975e90a586ceba1758c53d602d
cptgeo <georgekalyvianakis18@gmail.com>
2025-01-30 13:36:47 +0100

    Convert SignupRequest to class and extend to SignupAdminRequest with additional userRole info

---
+++ server/src/main/java/com/kalyvianakis/estiator/io/dto/SignupAdminRequest.java
@@ -0,0 +1,16 @@
+package com.kalyvianakis.estiator.io.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class SignupAdminRequest extends SignupRequest {
+    @NotBlank(message = "User role cannot be blank")
+    String userRole;
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+}
