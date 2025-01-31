4b445a822e32d4dce9926cbabd0938ad6bd6b5d4
cptgeo <georgekalyvianakis18@gmail.com>
2025-01-31 18:13:50 +0100

    Refactor useQueryReservations and useQueryTables hooks to accept structured parameters for improved flexibility

--- client/src/hooks/useQueryReservations.tsx
+++ client/src/hooks/useQueryReservations.tsx
@@ -2,14 +2,20 @@ import type { ReservationData } from "@core/types";
 import type { UseQueryResult } from "@tanstack/react-query";
 import { useQuery } from "@tanstack/react-query";
 import { getReq } from "@core/utils";
-import type { AxiosRequestConfig } from "axios";
 
 const queryKey = "reservations";
 
-export default function useQueryReservations<T = ReservationData[]>(interval?: number, params?: AxiosRequestConfig): UseQueryResult<T | undefined> {
+type Params = {
+  /** Will get the number of reservations; Extra params `dateTo` and `dateFrom` can be used to get reservations for specific date range */
+  count?: boolean;
+  dateFrom?: string;
+  dateTo?: string;
+}
+
+export default function useQueryReservations<T = ReservationData[]>(interval?: number, params?: Params): UseQueryResult<T | undefined> {
   const query = useQuery({
     queryKey: [queryKey, params],
-    queryFn: () => getReq<T>(queryKey, params),
+    queryFn: () => getReq<T>(queryKey, { params }),
     // stop refetching after encountering error
     refetchInterval: (query) => query.state.fetchFailureCount > 0 ? false : interval
   });
--- client/src/hooks/useQueryTables.tsx
+++ client/src/hooks/useQueryTables.tsx
@@ -2,14 +2,18 @@ import type { TableData } from "@core/types";
 import type { UseQueryResult } from "@tanstack/react-query";
 import { useQuery } from "@tanstack/react-query";
 import { getReq } from "@core/utils";
-import type { AxiosRequestConfig } from "axios";
 
 const queryKey = "tables";
 
-export default function useQueryTables<T = TableData[]>(interval?: number, params?: AxiosRequestConfig): UseQueryResult<T | undefined> {
+type Params = {
+  count?: boolean;
+  capacity?: boolean;
+}
+
+export default function useQueryTables<T = TableData[]>(interval?: number, params?: Params): UseQueryResult<T | undefined> {
   const query = useQuery({
     queryKey: [queryKey, params],
-    queryFn: () => getReq<T>(queryKey, params),
+    queryFn: () => getReq<T>(queryKey, { params }),
     // stop refetching after encountering error
     refetchInterval: (query) => query.state.fetchFailureCount > 0 ? false : interval
   });
