c5525960272a89bdfd33cf10d02548c72de20661
cptgeo <georgekalyvianakis18@gmail.com>
2025-01-31 16:02:35 +0100

    Add MonthReservations and TotalCapacity components to Dashboard for enhanced reservation statistics

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,18 +1,22 @@
 import type { ReactElement } from "react";
 import PageHeader from "@components/PageHeader/PageHeader";
 import ReservationWidget from "@components/Widget/Reservation";
+import MonthReservations from "@components/Minicard/MonthReservations";
+import TotalCapacity from "@components/Minicard/TotalCapacity";
 
 export default function DashboardPage(): ReactElement {
   return (
     <>
       <PageHeader
         heading="Dashboard"
-        subheading="Here you can have an overview of your business analytics, quick actions and more."
       />
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+      <div className="gap-7 flex flex-row flex-nowrap mb-5 w-full shrink-0 p-1 overflow-auto relative no-scrollbar">
+        <MonthReservations />
+        <TotalCapacity />
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-1">
         <div className="grid gap-4 md:col-span-2">
           <div className="h-auto max-w-full rounded-lg">
-            {/* Render widget here */}
             <ReservationWidget />
           </div>
         </div>
