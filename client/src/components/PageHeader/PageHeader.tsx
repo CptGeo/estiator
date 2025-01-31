1cf824f04027ca77887abe14170d047bf0fcc348
cptgeo <georgekalyvianakis18@gmail.com>
2025-01-31 16:02:13 +0100

    Adjust PageHeader font size for improved readability

--- client/src/components/PageHeader/PageHeader.tsx
+++ client/src/components/PageHeader/PageHeader.tsx
@@ -10,7 +10,7 @@ export default function PageHeader(props: Props): ReactElement {
 
   return (
     <div className="mb-5">
-      <h1 className="text-3xl tracking-wide drop-shadow-md font-light mb-0">{heading}</h1>
+      <h1 className="text-lg tracking-wide drop-shadow-md mb-0">{heading}</h1>
       {subheading && <p className="mt-0 text-xs text-slate-400">{subheading}</p>}
     </div>
   );
