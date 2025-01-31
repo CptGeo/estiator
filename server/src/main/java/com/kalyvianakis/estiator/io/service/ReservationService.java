c078299c5440b880673cb574a95e7347b884c76c
cptgeo <georgekalyvianakis18@gmail.com>
2025-01-31 16:02:46 +0100

    Add date filtering and count retrieval to ReservationController and ReservationService

--- server/src/main/java/com/kalyvianakis/estiator/io/service/ReservationService.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/service/ReservationService.java
@@ -1,7 +1,10 @@
 package com.kalyvianakis.estiator.io.service;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
+import com.kalyvianakis.estiator.io.enums.ReservationStatus;
 import com.kalyvianakis.estiator.io.utils.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,6 +31,14 @@ public class ReservationService implements IReservationService {
   @Override
   public Reservation get(Long id) throws ResourceNotFoundException {
     return reservationRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Reservation not found for ID: " + id));
+  }
+
+  public Long getCount() {
+    return reservationRepository.count();
+  }
+
+  public Long getCountByDateLessThanEqual(LocalDate date) {
+    return reservationRepository.countByDateLessThanEqual(date);
   }
 
   @Override
