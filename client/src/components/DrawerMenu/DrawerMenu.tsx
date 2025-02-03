bff3342414d1a307b4c8b5b8c545400763457b49
cptgeo <georgekalyvianakis18@gmail.com>
2025-02-03 17:15:41 +0100

    Update DrawerMenu to display company information from settings

--- client/src/components/DrawerMenu/DrawerMenu.tsx
+++ client/src/components/DrawerMenu/DrawerMenu.tsx
@@ -12,10 +12,12 @@ import TableIcon from "@components/Icons/TableIcon";
 import DashboardIcon from "@components/Icons/DashboardIcon";
 import PersonIcon from "@components/Icons/PersonIcon";
 import AdminOnly from "@components/AuthorizationWrappers/AdminOnly";
+import useQuerySettings from "@hooks/useQuerySettings";
 
 export default function DrawerMenu(): ReactElement {
   const navigate = useNavigate();
   const location = useLocation();
+  const { data: settings } = useQuerySettings();
 
   const { open, toggleDrawer } = useDrawer();
 
@@ -48,7 +50,7 @@ export default function DrawerMenu(): ReactElement {
   return (
     <div className={`transition-transform flex flex-col h-full fixed left-0 top-0 max-w-[300px] w-full z-50 bg-slate-800 shadow-2xl px-5 pt-5 pb-2 ${!open ? "translate-x-[-100%]" : ""}`}>
       <div className="mb-5">
-        <CompanyAvatar company={ { name: "Estiator.io", description: "Company description" }} />
+        {settings && <CompanyAvatar company={ { name: settings.businessName, description: settings.businessDescription }} />}
       </div>
       <div className="flex flex-col justify-between h-full">
         <div className="flex flex-col gap-2">
