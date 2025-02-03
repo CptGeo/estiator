6e134e86531296eea47f820e7d12cbe3e958bebe
cptgeo <georgekalyvianakis18@gmail.com>
2025-02-03 17:36:34 +0100

    Improve SettingsListItem styling

--- client/src/components/Settings/Item.tsx
+++ client/src/components/Settings/Item.tsx
@@ -7,11 +7,11 @@ export default function SettingsListItem(props: Props) {
   return (
     <div className="w-full flex flex-row">
       <div className="w-full grid grid-cols-1 gap-2 md:grid-cols-8 md:gap-10 border-b-1 border-foreground-200 py-5">
-        <div className="col-span-3 py-3 pl-1">
-          <h3 className="font-bold text-lg mb-1">{headline}</h3>
-          <p className="text-small text-foreground-400">{description}</p>
+        <div className="col-span-3 pl-1">
+          <h3 className="font-bold text-lg">{headline}</h3>
+          <p className="text-tiny lg:text-sm text-foreground-400">{description}</p>
         </div>
-        <div className="col-span-5 flex items-center">
+        <div className="col-span-5 flex">
           {children}
         </div>
       </div>
