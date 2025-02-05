2825518dfed472c8f79e2b76a566a8911885809a
cptgeo <georgekalyvianakis18@gmail.com>
2025-02-05 16:55:57 +0100

    Add methods to TableRepository for fetching free tables by date, time, duration, and reservation status

--- server/src/main/java/com/kalyvianakis/estiator/io/repository/TableRepository.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/repository/TableRepository.java
@@ -1,5 +1,6 @@
 package com.kalyvianakis.estiator.io.repository;
 
+import com.kalyvianakis.estiator.io.enums.ReservationStatus;
 import com.kalyvianakis.estiator.io.model.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -15,11 +16,42 @@ public interface TableRepository extends JpaRepository<Table, Long> {
     Long getTotalCapacity();
 
     @Query(
-        value = "SELECT DISTINCT tab.* FROM Tables AS tab EXCEPT\n"
-            + "     SELECT t.* from Tables AS t\n"
-            + "       INNER JOIN reservations AS r ON r.table_id = t.id\n"
-            + "     WHERE `date` = ?1\n"
-            + "         AND (TIME(?2) BETWEEN r.time AND (ADDDATE(r.`time`, INTERVAL r.duration SECOND) - 1))\n"
-            + "         OR  ADDDATE(TIME(?2), INTERVAL ?3 SECOND) BETWEEN r.time + 1 AND ADDDATE(r.`time`, INTERVAL r.duration SECOND)", nativeQuery = true)
-    List<Table> getIdsFreeByDateAndTimeAndDuration(LocalDate date, LocalTime time, Integer duration);
+            value = "SELECT DISTINCT tab.* FROM tables AS tab EXCEPT\n"
+                    + "SELECT t.* FROM tables t\n"
+                    + "INNER JOIN reservations r ON r.table_id = t.id\n"
+                    + "WHERE r.`date` = ?1\n"
+                    + "AND (\n"
+                    + "    -- Case 1: New reservation fully overlaps an existing one\n"
+                    + "    (TIME(?2) <= r.time \n"
+                    + "    AND ADDTIME(TIME(?2), SEC_TO_TIME(?3)) >= ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
+                    + "    \n"
+                    + "    -- Case 2: New reservation ends inside an existing one\n"
+                    + "    OR (ADDTIME(TIME(?2), SEC_TO_TIME(?3)) > r.time \n"
+                    + "    AND ADDTIME(TIME(?2), SEC_TO_TIME(?3)) <= ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
+                    + "    \n"
+                    + "    -- Case 3: New reservation starts inside an existing one\n"
+                    + "    OR (TIME(?2) >= r.time \n"
+                    + "    AND TIME(?2) < ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
+                    + ")", nativeQuery = true)
+    List<Table> getFreeByDateAndTimeAndDuration(LocalDate date, LocalTime time, Integer duration);
+
+    @Query(
+            value = "SELECT DISTINCT tab.* FROM tables AS tab EXCEPT\n"
+                    + "SELECT t.* FROM tables t\n"
+                    + "INNER JOIN reservations r ON r.table_id = t.id\n"
+                    + "WHERE r.`date` = ?1 and r.status IN ?4 \n"
+                    + "AND (\n"
+                    + "    -- Case 1: New reservation fully overlaps an existing one\n"
+                    + "    (TIME(?2) <= r.time \n"
+                    + "    AND ADDTIME(TIME(?2), SEC_TO_TIME(?3)) >= ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
+                    + "    \n"
+                    + "    -- Case 2: New reservation ends inside an existing one\n"
+                    + "    OR (ADDTIME(TIME(?2), SEC_TO_TIME(?3)) > r.time \n"
+                    + "    AND ADDTIME(TIME(?2), SEC_TO_TIME(?3)) <= ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
+                    + "    \n"
+                    + "    -- Case 3: New reservation starts inside an existing one\n"
+                    + "    OR (TIME(?2) >= r.time \n"
+                    + "    AND TIME(?2) < ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
+                    + ")", nativeQuery = true)
+    List<Table> getFreeByDateAndTimeAndDurationInStatus(LocalDate date, LocalTime time, Integer duration, List<ReservationStatus> status);
 }
