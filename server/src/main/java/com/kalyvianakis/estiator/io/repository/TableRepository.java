c2285603ae0c0957ce4ed9b78ee7c49a945d08fe
cptgeo <georgekalyvianakis18@gmail.com>
2025-02-10 19:40:22 +0100

    Add conflict counting methods in ReservationRepository and update TableRepository queries for improved reservation management

--- server/src/main/java/com/kalyvianakis/estiator/io/repository/TableRepository.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/repository/TableRepository.java
@@ -4,6 +4,7 @@ import com.kalyvianakis.estiator.io.enums.ReservationStatus;
 import com.kalyvianakis.estiator.io.model.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -19,39 +20,43 @@ public interface TableRepository extends JpaRepository<Table, Long> {
             value = "SELECT DISTINCT tab.* FROM tables AS tab EXCEPT\n"
                     + "SELECT t.* FROM tables t\n"
                     + "INNER JOIN reservations r ON r.table_id = t.id\n"
-                    + "WHERE r.`date` = ?1\n"
+                    + "WHERE r.`date` = :date\n"
                     + "AND (\n"
                     + "    -- Case 1: New reservation fully overlaps an existing one\n"
-                    + "    (TIME(?2) <= r.time \n"
-                    + "    AND ADDTIME(TIME(?2), SEC_TO_TIME(?3)) >= ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
-                    + "    \n"
+                    + "    (TIME(:time) <= r.time \n"
+                    + "    AND ADDTIME(TIME(:time), SEC_TO_TIME(:duration)) >= ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
                     + "    -- Case 2: New reservation ends inside an existing one\n"
-                    + "    OR (ADDTIME(TIME(?2), SEC_TO_TIME(?3)) > r.time \n"
-                    + "    AND ADDTIME(TIME(?2), SEC_TO_TIME(?3)) <= ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
-                    + "    \n"
+                    + "    OR (ADDTIME(TIME(:time), SEC_TO_TIME(:duration)) > r.time \n"
+                    + "    AND ADDTIME(TIME(:time), SEC_TO_TIME(:duration)) <= ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
                     + "    -- Case 3: New reservation starts inside an existing one\n"
-                    + "    OR (TIME(?2) >= r.time \n"
-                    + "    AND TIME(?2) < ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
+                    + "    OR (TIME(:time) >= r.time \n"
+                    + "    AND TIME(:time) < ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
                     + ")", nativeQuery = true)
-    List<Table> getFreeByDateAndTimeAndDuration(LocalDate date, LocalTime time, Integer duration);
+    List<Table> getFreeByDateAndTimeAndDuration(
+            @Param(value = "date") LocalDate date,
+            @Param(value = "time") LocalTime time,
+            @Param(value = "duration") Integer duration);
 
     @Query(
             value = "SELECT DISTINCT tab.* FROM tables AS tab EXCEPT\n"
                     + "SELECT t.* FROM tables t\n"
                     + "INNER JOIN reservations r ON r.table_id = t.id\n"
-                    + "WHERE r.`date` = ?1 and r.status IN ?4 \n"
+                    + "WHERE r.`date` = :date and r.status IN (:statuses) \n"
                     + "AND (\n"
                     + "    -- Case 1: New reservation fully overlaps an existing one\n"
-                    + "    (TIME(?2) <= r.time \n"
-                    + "    AND ADDTIME(TIME(?2), SEC_TO_TIME(?3)) >= ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
-                    + "    \n"
+                    + "    (TIME(:time) <= r.time \n"
+                    + "    AND ADDTIME(TIME(:time), SEC_TO_TIME(:duration)) >= ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
                     + "    -- Case 2: New reservation ends inside an existing one\n"
-                    + "    OR (ADDTIME(TIME(?2), SEC_TO_TIME(?3)) > r.time \n"
-                    + "    AND ADDTIME(TIME(?2), SEC_TO_TIME(?3)) <= ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
-                    + "    \n"
+                    + "    OR (ADDTIME(TIME(:time), SEC_TO_TIME(:duration)) > r.time \n"
+                    + "    AND ADDTIME(TIME(:time), SEC_TO_TIME(:duration)) <= ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
                     + "    -- Case 3: New reservation starts inside an existing one\n"
-                    + "    OR (TIME(?2) >= r.time \n"
-                    + "    AND TIME(?2) < ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
+                    + "    OR (TIME(:time) >= r.time \n"
+                    + "    AND TIME(:time) < ADDTIME(r.time, SEC_TO_TIME(r.duration)))\n"
                     + ")", nativeQuery = true)
-    List<Table> getFreeByDateAndTimeAndDurationInStatus(LocalDate date, LocalTime time, Integer duration, List<ReservationStatus> status);
+    List<Table> getFreeByDateAndTimeAndDurationExcludeStatus(
+            @Param(value = "date") LocalDate date,
+            @Param(value = "time") LocalTime time,
+            @Param(value = "duration") Integer duration,
+            @Param(value= "statuses") List<Short> status
+    );
 }
