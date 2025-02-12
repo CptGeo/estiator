6e405148908c37a927ac390eb5199be54f8b73a8
cptgeo <georgekalyvianakis18@gmail.com>
2025-02-12 18:12:42 +0100

    Add TableAvailabilityWidget to Dashboard for improved table occupancy insights

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -4,6 +4,7 @@ import ReservationWidget from "@components/Widget/Reservation";
 import MonthReservations from "@components/Minicard/MonthReservations";
 import TotalCapacity from "@components/Minicard/TotalCapacity";
 import TotalTables from "@components/Minicard/TotalTables";
+import TableAvailabilityWidget from "@components/Widget/TableAvailability";
 
 export default function DashboardPage(): ReactElement {
   return (
@@ -16,9 +17,12 @@ export default function DashboardPage(): ReactElement {
         <TotalTables />
         <TotalCapacity />
       </div>
-      <div className="grid grid-cols-4 md:grid-cols-12 gap-4 p-1">
-        <div className="gap-4 col-span-full sm:col-span-12 md:col-span-12 lg:col-span-10 xl:col-span-6">
+      <div className="grid grid-cols-4 md:grid-cols-12 gap-8 p-1">
+        <div className="gap-4 col-span-full sm:col-span-12 md:col-span-12 lg:col-span-10 xl:col-span-7">
           <ReservationWidget />
+        </div>
+        <div className="gap-4 col-span-full sm:col-span-8 md:col-span-9 lg:col-span-7 xl:col-span-4">
+          <TableAvailabilityWidget />
         </div>
       </div>
     </>
