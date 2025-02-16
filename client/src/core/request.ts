2391729f69793668f0f87a3c2dda86928dd1cef8
cptgeo <georgekalyvianakis18@gmail.com>
2025-02-16 19:32:51 +0100

    Update base URL for API requests to use local network address

--- client/src/core/request.ts
+++ client/src/core/request.ts
@@ -1,7 +1,7 @@
 import axios, { HttpStatusCode } from "axios";
 
 export const client = axios.create({
-  baseURL: "http://localhost:8888",
+baseURL: "http://192.168.1.194:8888",
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
