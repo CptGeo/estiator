d3039008473961d4637c54fda17da9308894e7f4
cptgeo <georgekalyvianakis18@gmail.com>
2025-02-19 18:08:22 +0100

    Fetch defaultRowsPerPage setting and pass it as a prop to ReservationsTable

--- client/src/pages/ReservationsManagement/ReservationsManagement.tsx
+++ client/src/pages/ReservationsManagement/ReservationsManagement.tsx
@@ -1,15 +1,19 @@
 import type { ReactElement } from "react";
 import PageHeader from "@components/PageHeader/PageHeader";
 import ReservationsTable from "@components/Reservations/Table";
+import useQuerySetting from "@hooks/useQuerySetting";
 
 export default function ReservationsManagementPage(): ReactElement {
+  // @todo: Put in context
+  const { data: defaultRowsPerPage } = useQuerySetting("defaultRowsPerPage");
+
   return (
     <>
       <PageHeader
         heading="Reservations management"
         subheading="Here you can manage existing or create new reservations for your business."
       />
-      <ReservationsTable />
+      {defaultRowsPerPage && <ReservationsTable defaultRowsPerPage={defaultRowsPerPage} />}
     </>
   );
 }
