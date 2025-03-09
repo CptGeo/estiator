59184478f34d183f39e0e15210ee0eada601d65a
cptgeo <georgekalyvianakis18@gmail.com>
2025-03-09 23:36:10 +0100

    Remove Footer component and adjust MainContent layout to reduce padding

--- client/src/layouts/Private/MainContent.tsx
+++ client/src/layouts/Private/MainContent.tsx
@@ -1,20 +1,18 @@
 import { Outlet } from "react-router-dom";
 import DrawerMenu from "@components/DrawerMenu/DrawerMenu";
 import Navigation from "@components/Navigation/Navigation";
-import Footer from "@components/Footer/Footer";
 import { useDrawer } from "@context/Drawer";
 
 export default function MainContent() {
   const { open } = useDrawer();
 
   return (
-    <div className={`main min-h-screen flex flex-col transition-all ${open ? "pl-[300px]" : ""}`}>
+    <div className={`main min-h-screen flex flex-col ${open ? "pl-[250px]" : "pl-[50px]"}`}>
       <Navigation />
       <DrawerMenu />
       <div className="container max-w-[1540px] mx-auto px-5 mt-7 flex-grow">
         <Outlet />
       </div>
-      <Footer />
     </div>
   )
 }
