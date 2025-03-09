5bc7e9f9dc0639dc9d7128d82388def20a9a5a15
cptgeo <georgekalyvianakis18@gmail.com>
2025-03-09 23:45:38 +0100

    Refactor CompanyAvatar component to improve class management and conditional rendering

--- client/src/components/Avatar/Company.tsx
+++ client/src/components/Avatar/Company.tsx
@@ -1,5 +1,6 @@
 import { User } from "@heroui/react";
 import type { CompanyData } from "@core/types";
+import classNames from "classnames";
 
 export default function CompanyAvatar(props: { company: CompanyData, menuOpen: boolean } ) {
   const { name, description } = props.company;
@@ -14,9 +15,16 @@ export default function CompanyAvatar(props: { company: CompanyData, menuOpen: b
         name: `${name.charAt(0)}${name.charAt(1)}`.toUpperCase()
       }}
       {...props.menuOpen && { description }}
-      {...props.menuOpen ? { name: name } : { name: "" }}
+      description={description}
+      name={name}
       className="transition-transform text-slate-300 items-start"
-      classNames={{ description: "text-left" }}
+      classNames={{
+        description: classNames([
+          "text-left",
+          !props.menuOpen ? "hidden" : ""
+        ]),
+        name: !props.menuOpen ? "hidden" : ""
+      }}
     />
   );
 }
