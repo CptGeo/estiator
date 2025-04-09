3e4ec6c8d3047403013ae74c8def7161c99c09ec
cptgeo <georgekalyvianakis18@gmail.com>
2025-04-10 01:13:01 +0200

    Refactor UserPatcher to accept AuthenticatedUser for patching user data

--- server/src/main/java/com/kalyvianakis/estiator/io/component/patcher/UserPatcher.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/component/patcher/UserPatcher.java
@@ -45,36 +45,8 @@ public class UserPatcher implements IPatcher<User>{
     if (incomplete.getTables() != null) {
       existing.setTables(incomplete.getTables());
     }
-  }
-
-  public void patch(User existing, AuthenticatedUser incomplete) {
-    if (incomplete.getName() != null) {
-      existing.setName(incomplete.getName());
-    }
-    if (incomplete.getSurname() != null) {
-      existing.setSurname(incomplete.getSurname());
-    }
-    if (incomplete.getPassword() != null) {
-      existing.setPassword(incomplete.getPassword());
-    }
-    if (incomplete.getPosition() != null) {
-      existing.setPosition(incomplete.getPosition());
-    }
-    if (incomplete.getEmail() != null) {
-      existing.setEmail(incomplete.getEmail());
-    }
-    if (incomplete.getPhone() != null) {
-      existing.setPhone(incomplete.getPhone());
-    }
-    if (incomplete.getStatus() != null) {
-      existing.setStatusValue(incomplete.getStatus().getLabel());
-      existing.setStatus(incomplete.getStatus());
-    }
-    if (incomplete.getUserRole() != null) {
-      existing.setUserRole(incomplete.getUserRole());
-    }
-    if (incomplete.getProfileImage() != null) {
-      existing.setProfileImage(incomplete.getProfileImage());
+    if (incomplete.getDietaryPreferences() != null) {
+      existing.setDietaryPreferences(incomplete.getDietaryPreferences());
     }
   }
 }
--- server/src/main/java/com/kalyvianakis/estiator/io/dto/AuthenticatedUser.java
+++ server/src/main/java/com/kalyvianakis/estiator/io/dto/AuthenticatedUser.java
@@ -83,4 +83,5 @@ public class AuthenticatedUser implements UserDetails {
 
     @Override
     public boolean isEnabled() { return true; }
+
 }
