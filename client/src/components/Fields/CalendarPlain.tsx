8d372e1b5f87a083b13f159757568d45f3d736fd
cptgeo <georgekalyvianakis18@gmail.com>
2025-05-18 16:33:57 +0200

    Enhance CalendarPlainField: add required prop handling for validation

--- client/src/components/Fields/CalendarPlain.tsx
+++ client/src/components/Fields/CalendarPlain.tsx
@@ -4,15 +4,17 @@ import { Controller } from "react-hook-form";
 
 type Props = {
     name: string;
+    required?: boolean;
 } & Omit<CalendarProps, "value" | "onChange" | "onBlur" >;
 
 export default function CalendarPlainField(props: Props) {
-    const { name, defaultValue, ...calendarProps } = props;
+    const { name, defaultValue, required = false, ...calendarProps } = props;
 
     return (
         <Controller
             name={name}
             defaultValue={defaultValue}
+            rules={{ required }}
             render={({ field: { onChange, onBlur, value, ref } }) => (
                 <Calendar
                     {...calendarProps}
