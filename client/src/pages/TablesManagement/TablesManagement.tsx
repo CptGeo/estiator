791b4b0c368bac9658ccc11f1cabf0317aa372a4
cptgeo <georgekalyvianakis18@gmail.com>
2025-05-18 19:53:39 +0200

    Wrap TablesManagementPage in TablesProvider for context management

--- client/src/pages/TablesManagement/TablesManagement.tsx
+++ client/src/pages/TablesManagement/TablesManagement.tsx
@@ -2,17 +2,18 @@ import type { ReactElement } from "react";
 import PageHeader from "@components/PageHeader/PageHeader";
 import TablesGrid from "@components/TablesGrid/TablesGrid";
 import config from "@settings";
+import TablesProvider from "@context/Tables";
 
 const GRID_SIZE = config.gridSize;
 
 export default function TablesManagementPage(): ReactElement {
   return (
-    <>
+    <TablesProvider>
       <PageHeader
         heading="Tables management"
         subheading="Here you can manage your restaurant tables and layout"
       />
       <TablesGrid size={GRID_SIZE}/>
-    </>
+    </TablesProvider>
   );
 }
