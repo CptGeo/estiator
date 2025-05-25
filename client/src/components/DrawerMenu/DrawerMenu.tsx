b8ec21aaaab448031ee6c5780474cd2f7262a06b
cptgeo <georgekalyvianakis18@gmail.com>
2025-05-25 22:05:21 +0200

    Refactor DrawerMenu component to use route constants and improve navigation matching logic

--- client/src/components/DrawerMenu/DrawerMenu.tsx
+++ client/src/components/DrawerMenu/DrawerMenu.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@heroui/react";
 import type { ReactElement } from "react";
-import type { Location, To } from "react-router-dom";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useMatch, useNavigate } from "react-router-dom";
 import CompanyAvatar from "@components/Avatar/Company";
 import { useDrawer } from "@context/Drawer";
 import AdminOnly from "@components/AuthorizationWrappers/AdminOnly";
@@ -10,28 +9,24 @@ import { BadgeTwoTone, CalendarMonthTwoTone, DashboardTwoTone, MenuOpenTwoTone, 
 import classNames from "classnames";
 import ModeratorOnly from "@components/AuthorizationWrappers/ModeratorOnly";
 import ClientOnly from "@components/AuthorizationWrappers/ClientOnly";
+import { allRoutes, getRootPage, Routes } from "@core/utils";
+import { useAuth } from "@context/Authentication";
 
 export default function DrawerMenu(): ReactElement {
-  const navigate = useNavigate();
-  const location = useLocation();
   const { data: settings } = useQuerySettings();
+  const auth = useAuth();
 
   const { open, toggleDrawer } = useDrawer();
 
-  function DrawerItem(props:  { to: string, icon: ReactElement, text: string, isIconOnly?: boolean } ): ReactElement {
-    const { to, icon, text, isIconOnly = false } = props;
+  function DrawerItem(props: { pattern?: string; to: string, icon: ReactElement, text: string, isIconOnly?: boolean, end?: boolean }): ReactElement {
+    const { pattern = "", to, icon, text, isIconOnly = false, end = false } = props;
+    const navigate = useNavigate();
 
-    function isCurrent(location: Location, to: To) {
-      const tokens = location.pathname.split("/").splice(1);
-
-      /** TODO: Needs to be improved. Returns `true` when at least one url token matches the current */
-      for(const token of tokens) {
-        if (token === to.toString().slice(1)) {
-          return true;
-        }
-      }
-      return false;
-    }
+    // Combine base path and optional pattern (e.g. "/admin/employees-management/*")
+    const match = useMatch({
+      path: to + pattern,
+      end
+    });
 
     return (
       <Button
@@ -39,11 +34,12 @@ export default function DrawerMenu(): ReactElement {
         variant="light"
         isIconOnly={isIconOnly}
         className={classNames([
-          " text-slate-300",
-          isCurrent(location, to) ? "bg-slate-600" : "",
-          !isIconOnly && "justify-start"
+          "text-slate-300",
+          match ? "bg-slate-600" : "",
+          !isIconOnly && "justify-start",
         ])}
-        startContent={icon}>
+        startContent={icon}
+      >
         {!isIconOnly && text}
       </Button>
     );
@@ -55,10 +51,10 @@ export default function DrawerMenu(): ReactElement {
       !open ? "px-1 w-[50px]" : "px-5 w-[250px]"
     ])}>
       <div className={classNames([
-          !open && "justify-center"
+        !open && "justify-center"
       ])}>
         <ModeratorOnly>
-          {settings && <CompanyAvatar menuOpen={open} company={ { name: settings.businessName, description: settings.businessDescription }} />}
+          {settings && <CompanyAvatar menuOpen={open} company={{ name: settings.businessName, description: settings.businessDescription }} />}
         </ModeratorOnly>
       </div>
       <div className="flex flex-col justify-between h-full pt-5">
@@ -67,17 +63,17 @@ export default function DrawerMenu(): ReactElement {
           !open && "items-center"
         ])}>
           <ClientOnly>
-            <DrawerItem to="/client-reservations" text="Reservations" isIconOnly={!open} icon={<CalendarMonthTwoTone className="text-xl" />} />
-            <DrawerItem to="/client-settings" text="Settings" isIconOnly={!open} icon={<SettingsTwoTone className="text-xl" />} />
+            <DrawerItem to={allRoutes[Routes.CLIENT_RESERVATIONS]} text="Reservations" isIconOnly={!open} icon={<CalendarMonthTwoTone className="text-xl" />} />
+            <DrawerItem to={allRoutes[Routes.CLIENT_SETTINGS]} text="Settings" isIconOnly={!open} icon={<SettingsTwoTone className="text-xl" />} />
           </ClientOnly>
           <ModeratorOnly>
-            <DrawerItem to="/" text="Dashboard" isIconOnly={!open} icon={<DashboardTwoTone className="text-xl" />} />
-            <DrawerItem to="/reservations-management" isIconOnly={!open} text="Reservations" icon={<CalendarMonthTwoTone className="text-xl" />} />
-            <DrawerItem to="/tables-management" isIconOnly={!open} text="Tables" icon={<TableRestaurantTwoTone className="text-xl" />} />
+            <DrawerItem end to={getRootPage(auth?.user?.userRole)} text="Dashboard" isIconOnly={!open} icon={<DashboardTwoTone className="text-xl" />} />
+            <DrawerItem to={allRoutes[Routes.RESERVATIONS]} isIconOnly={!open} text="Reservations" icon={<CalendarMonthTwoTone className="text-xl" />} />
+            <DrawerItem to={allRoutes[Routes.TABLES]} isIconOnly={!open} text="Tables" icon={<TableRestaurantTwoTone className="text-xl" />} />
             <AdminOnly>
-              <DrawerItem to="/employees-management" isIconOnly={!open} text="Employees" icon={<BadgeTwoTone className="text-xl" />} />
+              <DrawerItem pattern="/*" to={allRoutes[Routes.EMPLOYEES]} isIconOnly={!open} text="Employees" icon={<BadgeTwoTone className="text-xl" />} />
             </AdminOnly>
-            <DrawerItem to="/customers-management" isIconOnly={!open} text="Customers" icon={<PersonTwoTone className="text-xl" />} />
+            <DrawerItem to={allRoutes[Routes.CUSTOMERS]} isIconOnly={!open} text="Customers" icon={<PersonTwoTone className="text-xl" />} />
           </ModeratorOnly>
         </div>
 
@@ -86,7 +82,7 @@ export default function DrawerMenu(): ReactElement {
           !open && "items-center"
         ])}>
           <ModeratorOnly>
-            <DrawerItem to="/settings" text="Settings" isIconOnly={!open} icon={<SettingsTwoTone className="text-xl" />} />
+            <DrawerItem to={allRoutes[Routes.SETTINGS]} text="Settings" isIconOnly={!open} icon={<SettingsTwoTone className="text-xl" />} />
           </ModeratorOnly>
           <div className={classNames([
             "text-background border-t-1 border-background border-opacity-20 pt-2 text-center text-tiny text-nowrap",
